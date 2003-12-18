0fba89ab1585b828eedec7d9c9a69e09e02d6c30
Andreas Gruenbacher <agruen@suse.de>
2003-12-18 19:29:46 +0000

    Remove obsolete README

--- scripts/tar-up.sh
+++ scripts/tar-up.sh
@@ -16,7 +16,7 @@ fi
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 
-cp -pv README* kernel-source*.spec kernel-source*.changes \
+cp -pv kernel-source*.spec kernel-source*.changes \
 	series.conf config.conf scripts/config_subst.sh \
 	scripts/merge-headers scripts/running-kernel.init.in \
 	scripts/guards scripts/arch-symbols $BUILD_DIR
