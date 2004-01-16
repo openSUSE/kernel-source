438c87493ac87416a400449459a4005bd139737c
Andreas Schwab <schwab@suse.de>
2004-01-16 15:55:45 +0000

    Create all archs.

--- scripts/tar-up.sh
+++ scripts/tar-up.sh
@@ -2,28 +2,28 @@
 
 source $(dirname $0)/config.sh
 export LANG=POSIX
+SRC_FILE=linux-$VERSION.tar.bz2
+ARCHS="i386 ia64 ppc ppc64 s390x x86_64"
 
 while [ $# -gt 0 ]; do
-    case $1 in
-	--arch=*)
-	    export PATCH_ARCH="${1#--arch=}"
-	    ;;
-	--arch)
-	    export PATCH_ARCH="$2"
-	    shift
-	    ;;
-	*)
-	    break
-	    ;;
-    esac
-    shift
+  case $1 in
+    --arch=*)
+      ARCHS=${1#--arch=}
+      ;;
+    --arch)
+      ARCHS=$2
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+  shift
 done
 if [ $# -gt 0 ]; then
-    echo "SYNOPSIS: $0 [--arch=...]"
-    exit 1
+  echo "SYNOPSIS: $0 [--arch=...]"
+  exit 1
 fi
-
-SRC_FILE=linux-$VERSION.tar.bz2
 
 if [ -e scripts/check-conf ]; then
     scripts/check-conf || {
@@ -33,124 +33,125 @@ if [ -e scripts/check-conf ]; then
     }
 fi
 
-SYMBOLS=$(scripts/arch-symbols)
-echo "Architecture symbol(s): $SYMBOLS"
-if [ -s extra-symbols ]; then
-	EXTRA_SYMBOLS="$EXTRA_SYMBOLS $(cat extra-symbols)"
+rm -rf kernel-source-26
+mkdir kernel-source-26
+
+COMMON_FILES="kernel-source-26.changes \
+              series.conf config.conf scripts/merge-headers \
+	      scripts/check-for-config-changes \
+	      rpm/config_subst.sh rpm/running-kernel.init.in \
+	      rpm/functions.sh rpm/post.sh rpm/postun.sh \
+	      rpm/trigger-script.sh.in \
+	      scripts/guards scripts/arch-symbols"
+
+for d in config patches.*; do
+  # Skip non-directories. Also work around CVS problem: Directories can't
+  # be deleted, but are checked out with only a CVS sub-directory in them.
+  DIR_ENTRIES=( $d/* )
+  DIR_ENTRIES=${#DIR_ENTRIES[@]}
+  if [ ! -d $d -o $DIR_ENTRIES -eq 0 -o \
+       \( -d $d/CVS -a $DIR_ENTRIES -eq 1 \) ] ; then
+    echo "Skipping $d..."
+    continue
+  fi
+
+  echo "Compressing $d..."
+  tar -cf - --exclude=CVS --exclude="*~" --exclude=".#*" $d \
+    | bzip2 -1 > kernel-source-26/$d.tar.bz2
+  COMMON_FILES="$COMMON_FILES kernel-source-26/$d.tar.bz2"
+done
+if [ -r $SRC_FILE ]; then
+  LINUX_ORIG_TARBALL=$SRC_FILE
+elif [ -r $MIRROR/$SRC_FILE ]; then
+  LINUX_ORIG_TARBALL=$MIRROR/$SRC_FILE
+else
+  echo "Cannot find $SRC_FILE."
+  exit 1
 fi
-if [ -n "$EXTRA_SYMBOLS" ]; then
-    echo "Extra symbols: $EXTRA_SYMBOLS"
-    SYMBOLS="$SYMBOLS $EXTRA_SYMBOLS"
+if [ -r $LINUX_ORIG_TARBALL ]; then
+  cp -av $LINUX_ORIG_TARBALL kernel-source-26
+  COMMON_FILES="$COMMON_FILES kernel-source-26/${LINUX_ORIG_TARBALL##*/}"
 fi
 
-[ -z "$PATCH_ARCH" ] && PATCH_ARCH="$(uname -m)"
-case "$PATCH_ARCH" in
+[ -e skip-build ]    && COMMON_FILES="$COMMON_FILES skip-build" 
+[ -e extra-symbols ] && COMMON_FILES="$COMMON_FILES extra-symbols"
+
+for ARCH in $ARCHS; do
+  export PATCH_ARCH=$ARCH
+  SYMBOLS=$(scripts/arch-symbols)
+  echo "Architecture symbol(s): $SYMBOLS"
+  if [ -s extra-symbols ]; then
+    EXTRA_SYMBOLS="$EXTRA_SYMBOLS $(cat extra-symbols)"
+  fi
+  if [ -n "$EXTRA_SYMBOLS" ]; then
+    echo "Extra symbols: $EXTRA_SYMBOLS"
+    SYMBOLS="$SYMBOLS $EXTRA_SYMBOLS"
+  fi
+
+  case "$PATCH_ARCH" in
     (i?86)	PATCH_ARCH=i386 ;;
     (sun4u)	PATCH_ARCH=sparc64 ;;
     (arm*|sa110) PATCH_ARCH=arm ;;
     (s390x)	PATCH_ARCH=s390 ;;
     (parisc64)	PATCH_ARCH=parisc ;;
-esac
+  esac
 
-SUBARCH=$PATCH_ARCH
-BUILD_DIR="$PATCH_ARCH"
+  SUBARCH=$PATCH_ARCH
+  BUILD_DIR=kernel-source-26/$PATCH_ARCH
 
-rm -rf $BUILD_DIR
-mkdir -p $BUILD_DIR
+  mkdir $BUILD_DIR
 
-#echo "Copying various files..."
-cp -pv	kernel-source-26.changes \
-	series.conf config.conf scripts/merge-headers \
-	scripts/check-for-config-changes \
-	rpm/config_subst.sh rpm/running-kernel.init.in \
-	rpm/functions.sh rpm/post.sh rpm/postun.sh \
-	rpm/trigger-script.sh.in \
-	scripts/guards scripts/arch-symbols $BUILD_DIR
+  all_configs=($(scripts/guards --prefix config $SYMBOLS < config.conf))
+  # Filter out config/$ARCH/um (User Mode Linux): We do not pre-configure
+  # UML kernels.
+  configs=(${all_configs[*]/*\/um})
 
-[ -e skip-build ]    && cp -pv skip-build    $BUILD_DIR 
-[ -e extra-symbols ] && cp -pv extra-symbols $BUILD_DIR
-
-configs=( $(scripts/guards --prefix config $SYMBOLS < config.conf) )
-# Filter out config/$ARCH/um (User Mode Linux): We do not pre-configure
-# UML kernels.
-um_config="$(IFS=$'\n'
-	     echo "${configs[*]}" \
-	     | grep '/um$')"
-if [ -n "$um_config" ]; then
-    configs=( $(IFS=$'\n'
-    		echo "${configs[*]}" \
-		| grep -v '/um$') )
-fi
-
-for config in ${configs[@]} $um_config; do
+  for config in ${all_configs[*]}; do
     if [ $config = um ]; then
-	ARCH=um
+      ARCH=um
     else
-	ARCH=$SUBARCH
+      ARCH=$SUBARCH
     fi
 
     cfgname=${config##*/}
     echo "kernel-$cfgname-26.spec"
-    cat rpm/kernel-binary-26.spec.in \
-    | sed -e "s:@NAME@:kernel-$cfgname-26:g" \
-    	  -e "s:@ARCH@:$ARCH:g" -e "s:@SUBARCH@:$SUBARCH:g" \
-	  -e "s:@CFGNAME@:$cfgname:g" \
-	  -e "s:@VERSION@:$VERSION:g" \
-    | m4 > $BUILD_DIR/kernel-$cfgname-26.spec
+    sed -e "s:@NAME@:kernel-$cfgname-26:g" \
+    	-e "s:@ARCH@:$ARCH:g" -e "s:@SUBARCH@:$SUBARCH:g" \
+	-e "s:@CFGNAME@:$cfgname:g" \
+	-e "s:@VERSION@:$VERSION:g" \
+      < rpm/kernel-binary-26.spec.in \
+      | m4 > $BUILD_DIR/kernel-$cfgname-26.spec
     cat kernel-source-26.changes rpm/kernel-binary-26.changes \
-	> $BUILD_DIR/kernel-$cfgname-26.changes
-done
+      > $BUILD_DIR/kernel-$cfgname-26.changes
+  done
 
-# The pre-configured kernel source package
-if [ ${#configs[@]} -ne 0 ]; then
+  # The pre-configured kernel source package
+  if [ ${#configs[@]} -ne 0 ]; then
     cfgnames="${configs[@]##*/}"
     echo "kernel-source-26.spec"
-    cat rpm/kernel-source-26.spec.in \
-    | sed -e "s:@NAME@:kernel-source-26:g" \
-    	  -e "s:@ARCH@:$SUBARCH:g" \
-	  -e "s:@CFGNAMES@:$cfgnames:g" \
-	  -e "s:@VERSION@:$VERSION:g" \
-    | m4 > $BUILD_DIR/kernel-source-26.spec
-fi
+    sed -e "s:@NAME@:kernel-source-26:g" \
+    	-e "s:@ARCH@:$SUBARCH:g" \
+	-e "s:@CFGNAMES@:$cfgnames:g" \
+	-e "s:@VERSION@:$VERSION:g" \
+      < rpm/kernel-source-26.spec.in \
+      | m4 > $BUILD_DIR/kernel-source-26.spec
+  fi
 
-# The unconfigured kernel source package: Source for User Mode Linux, and
-# for any km_* packages that absolutely think they need kernel sources
-# installed.
-echo "kernel-bare-26.spec"
-cat rpm/kernel-source-26.spec.in \
-| sed -e "s:@NAME@:kernel-bare-26:g" \
+  # The unconfigured kernel source package: Source for User Mode Linux, and
+  # for any km_* packages that absolutely think they need kernel sources
+  # installed.
+  echo "kernel-bare-26.spec"
+  sed -e "s:@NAME@:kernel-bare-26:g" \
       -e "s:@ARCH@:$SUBARCH:g" \
       -e "s:@CFGNAMES@::g" \
       -e "s:@VERSION@:$VERSION:g" \
-| m4 > $BUILD_DIR/kernel-bare-26.spec
-cp kernel-source-26.changes $BUILD_DIR/kernel-bare-26.changes
+    < rpm/kernel-source-26.spec.in \
+    | m4 > $BUILD_DIR/kernel-bare-26.spec
+  cp kernel-source-26.changes $BUILD_DIR/kernel-bare-26.changes
 
-for d in config patches.*; do
-    # Skip non-directories. Also work around CVS problem: Directories can't
-    # be deleted, but are checked out with only a CVS sub-directory in them.
-    DIR_ENTRIES=( $d/* )
-    DIR_ENTRIES=${#DIR_ENTRIES[@]}
-    if [ ! -d $d -o $DIR_ENTRIES -eq 0 -o \
-	 \( -d $d/CVS -a $DIR_ENTRIES -eq 1 \) ] ; then
-	echo "Skipping $d..."
-	continue
-    fi
-
-    echo "Compressing $d..."
-    tar -cf - --exclude=CVS --exclude="*~" --exclude=".#*" $d \
-    | bzip2 -1 > $BUILD_DIR/$d.tar.bz2
+  ln ${COMMON_FILES} $BUILD_DIR
 done
-if [ -r $SRC_FILE ]; then
-	LINUX_ORIG_TARBALL=$SRC_FILE
-elif [ -r $MIRROR/$SRC_FILE ]; then
-	LINUX_ORIG_TARBALL=$MIRROR/$SRC_FILE
-else
-	echo "Cannot find $SRC_FILE."
-	exit 1
-fi
-if [ -r $LINUX_ORIG_TARBALL ]; then
-	cp -av $LINUX_ORIG_TARBALL $BUILD_DIR/
-	echo "done. build dir is now in ./$BUILD_DIR"
-else
-	echo "done, add $SRC_FILE to ./$BUILD_DIR"
+
+if [ ! -r $LINUX_ORIG_TARBALL ]; then
+  echo "Please add $SRC_FILE to kernel-source-26/*"
 fi
