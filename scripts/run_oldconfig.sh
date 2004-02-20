3ba6c6afea8c9fe449c32364c97a0d8bb42cffe0
Andreas Schwab <schwab@suse.de>
2004-02-20 15:30:17 +0000

    Make -y work.

--- scripts/run_oldconfig.sh
+++ scripts/run_oldconfig.sh
@@ -3,48 +3,47 @@ set -x
 
 ARCH="*"
 until [ "$#" = "0" ] ; do
-case "$1" in
-	y|-y|--yes)
-	YES="yes '' | "
-	shift
-	;;
-	a|-a|--arch)
-	shift
-	if [ -d arch/$1 -a "$1" != "" ] ; then
+  case "$1" in
+    y|-y|--yes)
+      YES="yes '' | "
+      shift
+      ;;
+    a|-a|--arch)
+      shift
+      if [ -d arch/$1 -a "$1" != "" ] ; then
 	ARCH=$1
 	shift
 	echo running make oldconfig only for arch $ARCH
-	else
+      else
 	echo "$1 is not a valid directory in arch/"
 	exit
-	fi
-	;;
-	*)
-	YES=""
-	shift
-	;;
-esac
+      fi
+      ;;
+    *)
+      YES=""
+      shift
+      ;;
+  esac
 done
-for i in arch/$ARCH/defconfig.*
-do
-#
-A=${i#*/}
-A=${A%%/*}
-C=${i##*/}
-B=${C#*.}
-#
-case "$i" in
-	*.um)
-	ARCH=um
-	;;
-	*)
-	ARCH=$A
-	;;
-esac
-echo $A ${A}_$C
-cp -v $i .config
-$YES make ARCH=$ARCH oldconfig
-cp -v .config ${A}_$C
-[ -f patches/config/$A/$B ] && cp -v .config patches/config/$A/$B
-diff -U0 $i .config
+for i in arch/$ARCH/defconfig.*; do
+  #
+  A=${i#*/}
+  A=${A%%/*}
+  C=${i##*/}
+  B=${C#*.}
+  #
+  case "$i" in
+    *.um)
+      ARCH=um
+      ;;
+    *)
+      ARCH=$A
+      ;;
+  esac
+  echo $A ${A}_$C
+  cp -v $i .config
+  eval $YES make ARCH=$ARCH oldconfig
+  cp -v .config ${A}_$C
+  [ -f patches/config/$A/$B ] && cp -v .config patches/config/$A/$B
+  diff -U0 $i .config
 done
