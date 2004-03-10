9b73153f35c4ed6430de92d14427799425a50ad3
Gerd Hoffmann <kraxel@suse.de>
2004-03-10 12:50:09 +0000

    - get rid of the annonying read in run_oldconfig, display the current   config processed on top of the screen instead.

--- scripts/run_oldconfig.sh
+++ scripts/run_oldconfig.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
-set -x
+#set -x
+
+#########################################################
+# dirty scroll region tricks ...
+
+use_region=0
+
+function _region_init_ () {
+	echo -e '\x1b[H\033[J'		# clear screen
+	echo -e '\x1b[4;0r'		# setup scroll region
+}
+
+function _region_fini_ () {
+	echo -ne '\x1b7'		# save cursor
+	echo -ne '\x1b[0;0r'		# del scroll region
+	echo -ne '\x1b8'		# restore cursor
+}
+
+function _region_msg_ () {
+	local msg="$*"
+	if test "$use_region" != "0"; then
+		echo -ne '\x1b7'	# save cursor
+		echo -ne '\x1b[0;0H'	# move cursoe
+		echo -e "##\x1b[K"	# message
+		echo -e "## $msg\x1b[K"	# message
+		echo -e "##\x1b[K"	# message
+		echo -ne '\x1b8'	# restore cursor
+	else
+		echo -ne "##"
+		echo -ne "## $msg"
+		echo -ne "##"
+	fi
+}
+
+case "$TERM" in
+	linux | xterm | screen)
+		use_region=1
+		_region_init_
+		trap "_region_fini_" EXIT
+		;;
+esac
+
+#########################################################
+# main
 
 ARCH="*"
 until [ "$#" = "0" ] ; do
@@ -42,8 +85,9 @@ for i in arch/$ARCH/defconfig.*; do
   esac
   echo $A ${A}_$C
   cp -v $i .config
-  echo working on $i, press RETURN
-  read
+#  echo working on $i, press RETURN
+#  read
+  _region_msg_ "working on $i"
   eval $YES make ARCH=$ARCH oldconfig
   cp -v .config ${A}_$C
   if [ -f patches/config/$A/$B ] \
