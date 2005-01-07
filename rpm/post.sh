c054888e129107dea6537f180be90712d801a4ec
Andreas Gruenbacher <agruen@suse.de>
2005-01-07 01:47:51 +0000

    - Fix bug in %post script: the vmlinuz symlink was not generated when   the kernel was installed by yast. For kernelum, instead of /boot/linux   and /boot/initrd use /boot/um-linux and /boot/um-initrd to lessen the   confusion. Don't create an initrd for kernel-um, yet: It's unclear   which modules to include; /etc/sysconfig/kernel:INITRD_MODULES does   not work with shared root filesystems (49443).

--- rpm/post.sh
+++ rpm/post.sh
@@ -1,41 +1,40 @@
 echo Setting up /lib/modules/%ver_str
 /sbin/depmod -a -F /boot/System.map-%ver_str %ver_str
 
-if [ "$YAST_IS_RUNNING" != instsys -a -n "$run_mkinitrd" ]; then
-    if [ -f /boot/vmlinuz-%ver_str ]; then
-	image=vmlinuz
-    elif [ -f /boot/image-%ver_str ]; then
-	image=image
-    elif [ -f /boot/vmlinux-%ver_str ]; then
-	image=vmlinux
-    fi
+initrd=initrd
+if [ -f /boot/vmlinuz-%ver_str ]; then
+    image=vmlinuz
+elif [ -f /boot/image-%ver_str ]; then
+    image=image
+elif [ -f /boot/vmlinux-%ver_str ]; then
+    image=vmlinux
+elif [ -f /boot/linux-%ver_str ]; then
+    # This is User Mode Linux; it may be installed on the same
+    # root filesystem as a native kernel. Make sure we don't mess
+    # up the native kernel's symlinks!
+    image=um-linux
+    initrd=um-initrd
+    run_mkinitrd=
+fi
 
+# Update the /boot/vmlinuz and /boot/initrd symlinks
+for x in /boot/$image /boot/$initrd; do
+    if [ -e $x -a "$(readlink $x)" != ${x##*/}-%ver_str ]; then
+	mv -f $x $x.previous
+    fi
+    rm -f $x
+    ln -s ${x##*/}-%ver_str $x
+done
+
+if [ "$YAST_IS_RUNNING" != instsys -a -n "$run_mkinitrd" ]; then
     if [ -f /etc/fstab ]; then
-	cd /boot
-	if ! /sbin/mkinitrd -k $image-%ver_str -i initrd-%ver_str; then
+	if ! /sbin/mkinitrd -k /boot/$image-%ver_str \
+			    -i /boot/$initrd-%ver_str; then
 	    echo "/sbin/mkinitrd failed" >&2
 	    exit 1
 	fi
     else
 	echo "please run mkinitrd as soon as your system is complete"
-    fi
-
-    # update /boot/vmlinuz symlink
-    if [ -L /boot/$image -a \
-	 "$(readlink /boot/$image)" != $image-%ver_str ]; then
-	mv /boot/$image /boot/$image.previous
-    fi
-    relink $image-%ver_str /boot/$image
-
-    # update /boot/initrd symlink
-    if [ -L /boot/initrd -a \
-	 "$(readlink /boot/initrd)" != initrd-%ver_str ]; then
-	mv /boot/initrd /boot/initrd.previous
-    fi
-    if [ -e /boot/initrd-%ver_str ]; then
-	relink initrd-%ver_str /boot/initrd
-    else
-	rm -f /boot/initrd
     fi
 
     if [ -x /sbin/new-kernel-pkg ]; then
