567a8ca76318ebe55767314fa5b5e9c326b36e8e
Andreas Gruenbacher <agruen@suse.de>
2005-01-12 12:27:42 +0000

    - rpm/kernel-source.spec.in: Fix "make silentoldconfig" invocation. - rpm/kernel-binary.spec.in: Add conflict between kernel-um and   other kernel-$BINARY packages. Add /usr/bin/linux symlink in   um-host-kernel. - rpm/post.sh: Small cleanup.

--- rpm/post.sh
+++ rpm/post.sh
@@ -1,24 +1,22 @@
 echo Setting up /lib/modules/%ver_str
 /sbin/depmod -a -F /boot/System.map-%ver_str %ver_str
 
-initrd=initrd
-if [ -f /boot/vmlinuz-%ver_str ]; then
-    image=vmlinuz
-elif [ -f /boot/image-%ver_str ]; then
-    image=image
-elif [ -f /boot/vmlinux-%ver_str ]; then
-    image=vmlinux
-elif [ -f /boot/linux-%ver_str ]; then
-    # This is User Mode Linux; it may be installed on the same
-    # root filesystem as a native kernel. Make sure we don't mess
-    # up the native kernel's symlinks!
-    image=um-linux
-    initrd=um-initrd
-    run_mkinitrd=
+for x in vmlinuz image vmlinux linux; do
+    if [ -f /boot/$x-%ver_str ]; then
+	image=$x
+	break
+    fi
+done
+if [ -z "$image" ]; then
+    exit 1
 fi
 
+# The User Mode Linux boot image is called "linux". It cannot be installed
+# on the same root filesystem as a native kernel, so we don't run into
+# a problem with the /boot/initrd symlink.
+
 # Update the /boot/vmlinuz and /boot/initrd symlinks
-for x in /boot/$image /boot/$initrd; do
+for x in /boot/$image /boot/initrd; do
     if [ -e $x -a "$(readlink $x)" != ${x##*/}-%ver_str ]; then
 	mv -f $x $x.previous
     fi
@@ -29,7 +27,7 @@ done
 if [ "$YAST_IS_RUNNING" != instsys -a -n "$run_mkinitrd" ]; then
     if [ -f /etc/fstab ]; then
 	if ! /sbin/mkinitrd -k /boot/$image-%ver_str \
-			    -i /boot/$initrd-%ver_str; then
+			    -i /boot/initrd-%ver_str; then
 	    echo "/sbin/mkinitrd failed" >&2
 	    exit 1
 	fi
