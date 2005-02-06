d28ac162131b7a5dda2e982416c6c03c42b6ec78
Kurt Garloff <garloff@suse.de>
2005-02-06 15:50:29 +0000

    Clean up patches.
    Remove: ide-no-unload, fork-interactive-balance.
    
    Rediff but leave disabled: clusterip-export-proc-file-op, struct-device,
    	vm_reserve-patch
    
    Rediff and reenable: qlogicisp-eh, scsi-format-unit-timeout,
    	keep-offsets.h, dynamic-timeslice, scsi-error-test-unit-ready-timeout,
    	scsi-scan-inq-ppc64-dflt

--- patches.rpmify/keep-offsets.h
+++ patches.rpmify/keep-offsets.h
@@ -1,23 +1,12 @@
-Index: linux-2.6.8/arch/ia64/Makefile
+From: garloff@suse.de
+Subject: Only remove offsets.h for make mrproper
+
+offsets.h does not need to be removed by make clean.
+
+Index: linux-2.6.10/arch/m68k/Makefile
 ===================================================================
---- linux-2.6.8.orig/arch/ia64/Makefile
-+++ linux-2.6.8/arch/ia64/Makefile
-@@ -83,9 +83,10 @@ unwcheck: vmlinux
- 
- archclean:
- 	$(Q)$(MAKE) $(clean)=$(boot)
- 
--CLEAN_FILES += include/asm-ia64/.offsets.h.stamp include/asm-ia64/offsets.h vmlinux.gz bootloader
-+CLEAN_FILES += include/asm-ia64/.offsets.h.stamp vmlinux.gz bootloader
-+MRPROPER_FILES += include/asm-ia64/offsets.h
- 
- prepare: include/asm-ia64/offsets.h
- 
- arch/ia64/kernel/asm-offsets.s: include/asm include/linux/version.h include/config/MARKER
-Index: linux-2.6.8/arch/m68k/Makefile
-===================================================================
---- linux-2.6.8.orig/arch/m68k/Makefile
-+++ linux-2.6.8/arch/m68k/Makefile
+--- linux-2.6.10.orig/arch/m68k/Makefile
++++ linux-2.6.10/arch/m68k/Makefile
 @@ -113,9 +113,9 @@ else
  	bzip2 -1c vmlinux >vmlinux.bz2
  endif
@@ -29,11 +18,11 @@ Index: linux-2.6.8/arch/m68k/Makefile
  arch/$(ARCH)/kernel/asm-offsets.s: include/asm include/linux/version.h \
  				   include/config/MARKER
  
-Index: linux-2.6.8/arch/parisc/Makefile
+Index: linux-2.6.10/arch/parisc/Makefile
 ===================================================================
---- linux-2.6.8.orig/arch/parisc/Makefile
-+++ linux-2.6.8/arch/parisc/Makefile
-@@ -93,10 +93,10 @@ arch/parisc/kernel/asm-offsets.s: includ
+--- linux-2.6.10.orig/arch/parisc/Makefile
++++ linux-2.6.10/arch/parisc/Makefile
+@@ -100,10 +100,10 @@ arch/parisc/kernel/asm-offsets.s: includ
  
  include/asm-parisc/offsets.h: arch/parisc/kernel/asm-offsets.s
  	$(call filechk,gen-asm-offsets)
@@ -46,11 +35,11 @@ Index: linux-2.6.8/arch/parisc/Makefile
  define archhelp
  	@echo  '* vmlinux	- Uncompressed kernel image (./vmlinux)'
  	@echo  '  palo		- Bootable image (./lifimage)'
-Index: linux-2.6.8/arch/ppc/Makefile
+Index: linux-2.6.10/arch/ppc/Makefile
 ===================================================================
---- linux-2.6.8.orig/arch/ppc/Makefile
-+++ linux-2.6.8/arch/ppc/Makefile
-@@ -129,8 +129,8 @@ ifneq ($(AS_ALTIVEC),0)
+--- linux-2.6.10.orig/arch/ppc/Makefile
++++ linux-2.6.10/arch/ppc/Makefile
+@@ -132,8 +132,8 @@ ifneq ($(AS_ALTIVEC),0)
  	@false
  endif
  	@true
@@ -61,22 +50,22 @@ Index: linux-2.6.8/arch/ppc/Makefile
  		$(TOUT)
 +MRPROPER_FILES += include/asm-$(ARCH)/offsets.h
  
-Index: linux-2.6.8/arch/ppc64/Makefile
+Index: linux-2.6.10/arch/ppc64/Makefile
 ===================================================================
---- linux-2.6.8.orig/arch/ppc64/Makefile
-+++ linux-2.6.8/arch/ppc64/Makefile
-@@ -79,5 +79,5 @@ define archhelp
+--- linux-2.6.10.orig/arch/ppc64/Makefile
++++ linux-2.6.10/arch/ppc64/Makefile
+@@ -91,5 +91,5 @@ define archhelp
    echo  '		   sourced from arch/$(ARCH)/boot/ramdisk.image.gz'
    echo  '		   (arch/$(ARCH)/boot/zImage.initrd)'
  endef
  
 -CLEAN_FILES += include/asm-ppc64/offsets.h
 +MRPROPER_FILES += include/asm-ppc64/offsets.h
-Index: linux-2.6.8/arch/s390/Makefile
+Index: linux-2.6.10/arch/s390/Makefile
 ===================================================================
---- linux-2.6.8.orig/arch/s390/Makefile
-+++ linux-2.6.8/arch/s390/Makefile
-@@ -95,9 +95,9 @@ arch/$(ARCH)/kernel/asm-offsets.s: inclu
+--- linux-2.6.10.orig/arch/s390/Makefile
++++ linux-2.6.10/arch/s390/Makefile
+@@ -107,9 +107,9 @@ arch/$(ARCH)/kernel/asm-offsets.s: inclu
  
  include/asm-$(ARCH)/offsets.h: arch/$(ARCH)/kernel/asm-offsets.s
  	$(call filechk,gen-asm-offsets)
