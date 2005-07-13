f13a39cdae32d7b72314d3afe219ce072ccd27e8
Olaf Hering <olh@suse.de>
2005-07-13 07:21:21 +0000

    really! -PATCHVERSION=2.6.13-rc2-git5 +PATCHVERSION=2.6.13-rc3

--- scripts/config.sh
+++ scripts/config.sh
@@ -10,6 +10,6 @@ fi
 # The version of the main tarball to use
 SRCVERSION=2.6.12
 # The version of the patched tree
-PATCHVERSION=2.6.13-rc2-git5
+PATCHVERSION=2.6.13-rc3
 BUILD_DIR=kernel-source
 IGNORE_ARCHS=
