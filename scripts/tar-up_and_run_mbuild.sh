fabb04ecbb9f2ffcc1f3f99253d40f48d5fe04e9
Andreas Schwab <schwab@suse.de>
2005-08-05 10:44:59 +0000

    Drop kernel-64k-pagesize.

--- scripts/tar-up_and_run_mbuild.sh
+++ scripts/tar-up_and_run_mbuild.sh
@@ -3,7 +3,7 @@
 echo "dont forget to update the defconfig files, or the mbuild may fail"
 sleep 1
 sudo -l
-important_specfiles="default smp ppc64 iseries64 s390 s390x 64k-pagesize"
+important_specfiles="default smp ppc64 iseries64 s390 s390x"
 all_specfiles="`sed -e '/^\+/s@^.*/@@p;d' config.conf | sort -u`"
 timestamp=
 tolerate_unknown_new_config_options=
