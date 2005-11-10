0555492dbb8e4730498143f3d65616c0d5892573
Olaf Hering <olh@suse.de>
2005-11-10 10:34:56 +0000

    remove hints which noone cares about anyway

--- scripts/tar-up.sh
+++ scripts/tar-up.sh
@@ -366,5 +366,3 @@ EOF
 fi
 chmod a+rx $BUILD_DIR/get_release_number.sh
 
-echo "If you want to submit the kernel to Autobuild now,"
-echo "how about running \` ./scripts/tar-up_and_run_mbuild.sh -l talk ' before?"
