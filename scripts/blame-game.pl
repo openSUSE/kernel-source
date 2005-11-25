5407753fd9f1921a99605d372bef1ceefc433bd4
Olaf Kirch <okir@suse.de>
2005-11-25 14:51:50 +0000

    pick up suse kernel person from Patch header if possible; fall back to CVS if not

--- scripts/blame-game.pl
+++ scripts/blame-game.pl
@@ -16,16 +16,49 @@ while (<>) {
 		$rest = $2;
 		$user = "anyone";
 
-		open LOG, "cvs log $name |" or die "Unable to run cvs log: $!\n";
-		while (<LOG>) {
-			# date: 2004-09-30 09:11:50 +0000;  author: olh;  state: Exp;
-			next unless (/^date.*author:\s*(\S+);/o);
-			$user = $1;
+		$user = &get_author($name);
+		if (!$user) {
+			$user = &get_committer($name);
 		}
-		close LOG;
 
-		printf "+%-6s\t$name$rest\n", $user;
+		printf "+%s\t$name$rest\n", $user;
 		next;
 	}
 	print "$line\n";
+}
+
+sub get_author {
+
+	local($patch) = @_;
+	my $user = '';
+
+	open PATCH, "<$patch" or return '';
+	while (<PATCH>) {
+		last if (/^--- /o);
+		if (/^From: (.*\@suse.*)/o
+		 || /^Signed-off-by: (.*\@suse.*)/o
+		 || /^Acked-by: (\S.*\@suse.*)/o) {
+		 	$user = $1;
+			$user =~ s/.*<([^>]*)>.*/\1/o;
+			$user =~ s/\@.*//o;
+			last;
+		}
+	}
+	close PATCH;
+	return $user;
+}
+
+sub get_committer {
+	local($patch) = @_;
+	my $user = '';
+
+	open LOG, "cvs log $patch |" or die "Unable to run cvs log: $!\n";
+	while (<LOG>) {
+		# date: 2004-09-30 09:11:50 +0000;  author: olh;  state: Exp;
+		next unless (/^date.*author:\s*(\S+);/o);
+		$user = $1;
+	}
+	close LOG;
+
+	return $user;
 }
