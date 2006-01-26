8837ebb395869f8df67e690d1324ee5114065e88
Andreas Gruenbacher <agruen@suse.de>
2006-01-26 15:31:56 +0000

    - rpm/prepare-build.sh: Ensure that we don't generate an empty   BuidRequires line.

--- rpm/prepare-build.sh
+++ rpm/prepare-build.sh
@@ -5,10 +5,10 @@ set -e
 test -f /.buildenv || exit 0
 this_release=`rpm -q --qf %{RELEASE} kernel-dummy`
 echo this_release $this_release
-test -z "$this_release" && exit 1
-for i in /usr/src/packages/SOURCES/*.spec
-do
-	test -f $i || continue
-	sed -e '/^BuildRequires:/s@kernel-dummy@@g' -e "/^Release:/s@^.*@Release: $this_release@" < $i > $i.$$
-	mv -v $i.$$ $i
+test -n "$this_release" || exit 1
+shopt -s nullglob
+for i in /usr/src/packages/SOURCES/*.spec; do
+	sed -i -e '/^BuildRequires:/s@kernel-dummy@@g' \
+	       -e '/^BuildRequires:[ \t]*$/d' \
+	       -e "/^Release:/s@^.*@Release: $this_release@" $i
 done
