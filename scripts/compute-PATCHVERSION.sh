655148d6160db51233f2b0a8a1f771d0532a8ed0
Andreas Gruenbacher <agruen@suse.de>
2007-08-23 12:32:33 +0000

    - Fix the release number computation and add some additional consistency checks.

--- scripts/compute-PATCHVERSION.sh
+++ scripts/compute-PATCHVERSION.sh
@@ -10,10 +10,12 @@ PATCHLEVEL=$2
 SUBLEVEL=$3
 EXTRAVERSION=
 
+EXTRA_SYMBOLS=$([ -e extra-symbols ] && cat extra-symbols)
+
 # Parse all the changes to KERNELRELEASE out of all patches and
 # convert them to shell code that can be evaluated. Evaluate it.
 eval "$(
-    scripts/guards < series.conf \
+    scripts/guards $EXTRA_SYMBOLS < series.conf \
     | xargs awk '
     /^--- |^+++ / \
 	{ M = match($2, /^[^\/]+\/Makefile( \t|$)/) }
