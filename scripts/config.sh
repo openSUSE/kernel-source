067de059064e53212f366f9fca351f9c76a7f454
Andreas Schwab <schwab@suse.de>
2007-11-21 14:44:47 +0000

    Remove reference to kraxel's home.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,12 +1,4 @@
-
-if [ -d /home/ftp/pub/kernel ]; then
-	# kraxel @home
-	MIRROR=${MIRROR:-/home/ftp/pub/kernel}
-else
-	# SuSE DHS
-	MIRROR=${MIRROR:-/mounts/mirror/kernel/v2.6}
-fi
-
+MIRROR=${MIRROR:-/mounts/mirror/kernel/v2.6}
 # The version of the main tarball to use
 SRCVERSION=2.6.23
 BUILD_DIR=kernel-source
