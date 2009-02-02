4558271261ee6c7de2d225fd0818052f859a28d8
Jeff Mahoney <jeffm@suse.de>
2009-02-02 11:36:17 -0500

    - Updated to 2.6.29-rc3.   - AppArmor is disabled.   - Xen is disabled.   - Eliminated 745 patches.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,8 +1,8 @@
 MIRROR=${MIRROR:-/mounts/mirror/kernel/v2.6}
 # The version of the main tarball to use
-SRCVERSION=2.6.27
+SRCVERSION=2.6.28
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 BUILD_DIR=kernel-source${VARIANT}
 IGNORE_ARCHS=
-DIST_SET=sle11
+DIST_SET=
