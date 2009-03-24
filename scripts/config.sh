4bf28205c0f7e5acf2f5690f0515b74f3460a7db
Jeff Mahoney <jeffm@suse.de>
2009-03-24 10:38:18 -0400

    - Update to 2.6.29-final.   - Eliminated 4 patches.

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,7 +3,7 @@ if test ! -d "$MIRROR" -a -d /cml/mirror/kernel_v2.6; then
     MIRROR=/cml/mirror/kernel_v2.6
 fi
 # The version of the main tarball to use
-SRCVERSION=2.6.28
+SRCVERSION=2.6.29
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 BUILD_DIR=kernel-source${VARIANT}
