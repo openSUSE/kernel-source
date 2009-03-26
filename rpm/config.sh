0045b526010cc5749fe5e2759ff6360d23615ce5
Michal Marek <mmarek@suse.cz>
2009-03-26 21:30:44 +0100

    - rpm/config.sh: introduce rpm/config.sh, defining SRCVERSION and   VARIANT variables.

---
+++ rpm/config.sh
@@ -0,0 +1,4 @@
+# The version of the main tarball to use
+SRCVERSION=2.6.29
+# variant of the kernel-source package, either empty or "-rt"
+VARIANT=
--- scripts/config.sh
+++ scripts/config.sh
@@ -2,10 +2,8 @@ MIRROR=${MIRROR:-/mounts/mirror/kernel/v2.6}
 if test ! -d "$MIRROR" -a -d /cml/mirror/kernel_v2.6; then
     MIRROR=/cml/mirror/kernel_v2.6
 fi
-# The version of the main tarball to use
-SRCVERSION=2.6.29
-# variant of the kernel-source package, either empty or "-rt"
-VARIANT=
+# SRCVERSION and VARIANT are set in rpm/config.sh
+source $(dirname "$0")/../rpm/config.sh
 BUILD_DIR=kernel-source${VARIANT}
 IGNORE_ARCHS=
 DIST_SET=
