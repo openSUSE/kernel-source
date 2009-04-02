b8a2d027bd88f3d526d3b43724755d955e55ec90
Michal Marek <mmarek@suse.cz>
2009-04-02 19:12:53 +0200

    - scripts/config.sh: don't fail if ../rpm/config.sh does not exits   (kbuild needs this)

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,7 +3,9 @@ if test ! -d "$MIRROR" -a -d /cml/mirror/kernel_v2.6; then
     MIRROR=/cml/mirror/kernel_v2.6
 fi
 # SRCVERSION and VARIANT are set in rpm/config.sh
-source $(dirname "$0")/../rpm/config.sh
+if test -e $(dirname -- "$0")/../rpm/config.sh; then
+    source "$_"
+fi
 BUILD_DIR=kernel-source${VARIANT}
 IGNORE_ARCHS=
 DIST_SET=
