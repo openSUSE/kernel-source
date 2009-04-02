09cf8debcb153c02c366f20e07926dbf9a58f9d9
Michal Marek <mmarek@suse.cz>
2009-04-02 17:51:07 +0200

    - rpm/mkspec: new script to generate spec files from *.spec.in   templates - rpm/compute-PATCHVERSION.sh, rpm/kernel-binary.spec.in,   rpm/kernel-source.spec.in, rpm/kernel-syms.spec.in: add to the   source rpm - scripts/tar-up.sh: just tar up patches directories and call   mkspec

--- rpm/compute-PATCHVERSION.sh
+++ rpm/compute-PATCHVERSION.sh
@@ -2,6 +2,15 @@
 
 export LANG=POSIX
 
+if ! test -d patches.kernel.org; then
+	if test -e patches.kernel.org.tar.bz2; then
+		tar xjf patches.kernel.org.tar.bz2
+		trap 'rm -rf patches.kernel.org' EXIT
+	else
+		echo "Can't find patches.kernel.org" >&2
+	fi
+fi
+
 source $(dirname $0)/config.sh
 set -- $(echo $SRCVERSION | sed -ne 's/\([0-9]\+\).\([0-9]\+\).\([0-9]\+\)\(.*\)/\1 \2 \3 \4/p')
 
@@ -10,12 +19,12 @@ PATCHLEVEL=$2
 SUBLEVEL=$3
 EXTRAVERSION=$4
 
-EXTRA_SYMBOLS=$(set -- $([ -e $(dirname $0)/../extra-symbols ] && cat $(dirname $0)/../extra-symbols) ; echo $*)
+EXTRA_SYMBOLS=$(set -- $([ -e $(dirname $0)/extra-symbols ] && cat $(dirname $0)/extra-symbols) ; echo $*)
 
 # Parse all the changes to KERNELRELEASE out of all patches and
 # convert them to shell code that can be evaluated. Evaluate it.
 eval "$(
-    scripts/guards $EXTRA_SYMBOLS < series.conf \
+    $(dirname $0)/guards $EXTRA_SYMBOLS < series.conf | grep '^patches\.kernel\.org' \
     | xargs awk '
     /^--- |^+++ / \
 	{ M = match($2, /^[^\/]+\/Makefile( \t|$)/) }
--- scripts/tar-up.sh
+++ scripts/tar-up.sh
@@ -2,9 +2,6 @@
 
 . ${0%/*}/wd-functions.sh
 
-rpm_release_timestamp=
-rpm_release_string=
-source_timestamp=
 tolerate_unknown_new_config_options=0
 ignore_kabi=
 ignore_unsupported_deps=
@@ -34,40 +31,15 @@ until [ "$#" = "0" ] ; do
       ignore_unsupported_deps=1
       shift
       ;;
-    -rs|--release-string)
-      case "$2" in
-      *[' '-]*)
-        echo "$1 option argument must not contain dashes or spaces" >&2
-	exit 1
-	;;
-      esac
-      rpm_release_string="$2"
-      shift 2
-      ;;
-    -ts|--timestamp)
-      rpm_release_timestamp=yes
-      shift
-      ;;
-    -k|--kbuild|--source-timestamp)
-      source_timestamp=1
-      shift
-      ;;
-    -u|--update-only)
-      update_only=1
-      shift
-      ;;
     -h|--help|-v|--version)
 	cat <<EOF
 
 ${0##*/} perpares a 'kernel-source' package for submission into autobuild
 
 these options are recognized:
-    -rs <string>       to append specified string to rpm release number
-    -ts                to use the current date as rpm release number
     -nf                to proceed if a new unknown .config option is found during make oldconfig
     -u		       update generated files in an existing kernel-source dir
     -i                 ignore kabi failures
-    --source-timestamp to autogenerate a release number based on branch and timestamp (overrides -rs/-ts)
 
 EOF
 	exit 1
@@ -81,28 +53,6 @@ done
 source $(dirname $0)/config.sh
 export LANG=POSIX
 SRC_FILE=linux-$SRCVERSION.tar.bz2
-PATCHVERSION=$($(dirname $0)/compute-PATCHVERSION.sh)
-
-case "$PATCHVERSION" in
-*-*)
-    RPMVERSION=${PATCHVERSION%%-*}
-    RELEASE_PREFIX=${PATCHVERSION#*-}.
-    RELEASE_PREFIX=${RELEASE_PREFIX//-/.}
-    ;;
-*)
-    RPMVERSION=$PATCHVERSION
-    RELEASE_PREFIX=
-    ;;
-esac
-
-if [ -n "$rpm_release_timestamp" ]; then
-    if test $(( ${#RPMVERSION} + 10 + 2 + 8 + ${#rpm_release_string})) -gt 64
-    then
-    	echo "${RPMVERSION}-${rpm_release_string}-\${flavour} exceeds the 64 byte 'uname -r' limit. Use a shorter string."
-	exit 1
-    fi
-    rpm_release_string="\`env -i - TZ=GMT date +%Y%m%d\`${rpm_release_string:+_$rpm_release_string}"
-fi
 
 [ -z "$build_dir" ] && build_dir=$BUILD_DIR
 if [ -z "$build_dir" ]; then
@@ -118,15 +68,8 @@ check_for_merge_conflicts() {
     fi
 }
 
-if [ -n "$update_only" ]; then
-	if [ ! -d "$build_dir" ]; then
-		echo "$build_dir must exist in update mode."
-		exit 1
-	fi
-else
-	rm -rf $build_dir
-	mkdir -p $build_dir
-fi
+rm -rf $build_dir
+mkdir -p $build_dir
 
 # generate the list of patches to include.
 if [ -n "$embargo_filter" ]; then
@@ -165,116 +108,7 @@ if $using_git; then
     if test -n "$tag"; then
 	echo "GIT Branch: $tag" >>$build_dir/build-source-timestamp
     fi
-    git_commit=$(git rev-parse HEAD)
-    git_commit=${git_commit:0:8}
 fi
-
-if [ -n "$source_timestamp" ]; then
-	ts="$(head -n 1 $build_dir/build-source-timestamp)"
-	branch=$(sed -nre 's/^GIT Branch: //p' \
-		 $build_dir/build-source-timestamp)
-	rpm_release_string=${branch}_$(date --utc '+%Y%m%d%H%M%S' -d "$ts")
-fi
-
-sed -e "s:@RELEASE_PREFIX@:$RELEASE_PREFIX:"     \
-    -e "s:@RELEASE_SUFFIX@:$rpm_release_string:" \
-    -e "s:@COMMIT@:$git_commit:"                 \
-    rpm/get_release_number.sh.in                 \
-    > $build_dir/get_release_number.sh
-chmod 755 $build_dir/get_release_number.sh
-RELEASE=$($build_dir/get_release_number.sh)
-
-# List all used configurations
-config_files="$(
-    for arch in $(scripts/arch-symbols --list) ; do
-	scripts/guards $arch < config.conf \
-	| sed -e "s,^,$arch ,"
-    done)"
-flavors="$(echo "$config_files" | sed -e 's,.*/,,' | sort -u)"
-
-for flavor in $flavors ; do
-    echo "kernel-$flavor.spec"
-
-    # Find all architectures for this spec file
-    set -- $(
-	echo "$config_files" \
-	| sed -e "/\/$flavor/!d" \
-	      -e "s, .*,,g" \
-	| sort -u)
-    archs="$*"
-    
-    # Compute @PROVIDES_OBSOLETES@ expansion
-    head=""
-    for arch in $archs ; do
-	p=( $(scripts/guards $arch $flavor p \
-		< rpm/old-packages.conf) )
-	o=( $(scripts/guards $arch $flavor o \
-		< rpm/old-packages.conf) )
-
-	# Do we have an override config file or an additional patch?
-	if [ -e $arch-$flavor.conf ]; then
-	    echo "Override config: $arch-$flavor.conf"
-	    cp $arch-$flavor.conf $build_dir/
-	fi
-	if [ -e $arch-$flavor.diff ]; then
-	    echo "Extra patch: $arch-$flavor.diff"
-	    cp $arch-$flavor.diff $build_dir/
-	fi
-
-	[ $arch = i386 ] && arch="%ix86"
-	nl=$'\n'
-	if [ -n "$p" -o -n "$p" ]; then
-	    head="${head}%ifarch $arch$nl"
-	    [ -n "$p" ] && head="${head}Provides:     ${p[@]}$nl"
-	    [ -n "$o" ] && head="${head}Obsoletes:    ${o[@]}$nl"
-	    head="${head}%endif$nl"
-	fi
-    done
-    prov_obs="${head%$'\n'}"
-
-    # If we build this spec file for only one architecture,  the
-    # enclosing if is not needed
-    if [ $(set -- $archs ; echo $#) -eq 1 ]; then
-	prov_obs="$(echo "$prov_obs" | grep -v '%ifarch\|%endif')"
-    fi
-
-    # In ExclusiveArch in the spec file, we must specify %ix86 instead
-    # of i386.
-    archs="$(echo $archs | sed -e 's,i386,%ix86,g')"
-
-    # Generate spec file
-    sed -e "s,@NAME@,kernel-$flavor,g" \
-	-e "s,@FLAVOR@,$flavor,g" \
-	-e "s,@VARIANT@,$VARIANT,g" \
-	-e "s,@SRCVERSION@,$SRCVERSION,g" \
-	-e "s,@PATCHVERSION@,$PATCHVERSION,g" \
-	-e "s,@RPMVERSION@,$RPMVERSION,g" \
-	-e "s,@ARCHS@,$archs,g" \
-	-e "s,@PROVIDES_OBSOLETES@,${prov_obs//$'\n'/\\n},g" \
-	-e "s,@TOLERATE_UNKNOWN_NEW_CONFIG_OPTIONS@,$tolerate_unknown_new_config_options,g" \
-	-e "s,@RELEASE@,$RELEASE,g" \
-      < rpm/kernel-binary.spec.in \
-    > $build_dir/kernel-$flavor.spec
-done
-
-install_changes() {
-    local changes=$1
-    cat kernel-source.changes kernel-source.changes.old > $changes
-    chmod 644 $changes
-}
-
-for flavor in $flavors ; do
-    install_changes $build_dir/kernel-$flavor.changes
-done
-
-binary_spec_files=$(
-    n=50
-    for flavor in syms $flavors ; do
-	printf "%-14s%s\n" "Source$n:" "kernel-$flavor.spec"
-	n=$[$n+1]
-    done
-)
-binary_spec_files=${binary_spec_files//$'\n'/\\n}
 
 CLEANFILES=()
 trap 'if test -n "$CLEANFILES"; then rm -rf "${CLEANFILES[@]}"; fi' EXIT
@@ -283,136 +117,16 @@ CLEANFILES=("${CLEANFILES[@]}" "$tmpdir")
 
 EXTRA_SYMBOLS=$([ -e extra-symbols ] && cat extra-symbols)
 
-# Compute @REQUIRES@ expansion
-prepare_source_and_syms() {
-    local name=$1
-    local head="" nl ARCH_SYMBOLS packages flavor av arch
-
-    requires=
-
-    # Exclude flavors that have a different set of patches: we assume
-    # that the user won't change series.conf so much that two flavors
-    # that differ at tar-up.sh time will become identical later.
-    set -- $EXTRA_SYMBOLS
-    case $name in
-    (*-rt)
-	set -- RT "$@"
-	;;
-    esac
-    scripts/guards "$@" < series.conf > $tmpdir/$name.patches
-
-    for flavor in $flavors; do
-	build_archs=
-	for arch in $(scripts/arch-symbols --list); do
-	    arch_flavor=$(scripts/guards $arch < config.conf | grep "/$flavor$")
-	    [ -z "$arch_flavor" ] && continue
-	    av="${arch}_${flavor}"
-	    [ "$arch" = i386 ] && arch="%ix86"
-	    set -- kernel-$flavor $flavor \
-		   $(case $flavor in (rt|rt_*) echo RT ;; esac)
-
-	    # The patch selection for kernel-vanilla is a hack.
-	    [ $flavor = vanilla ] && continue
-
-	    scripts/guards $* $EXTRA_SYMBOLS < series.conf \
-		> $tmpdir/kernel-$av.patches
-	    diff -q $tmpdir/{$name,kernel-$av}.patches > /dev/null \
-		|| (echo skipping $av ; continue)
-	    build_archs="$build_archs $arch"
-	done
-
-	build_archs=${build_archs# }
-
-	if [ -n "$build_archs" ]; then
-	    nl=$'\n'
-	    or='%'
-	    head="${head}%ifarch ${build_archs// / || ifarch }$nl"
-	    head="${head}Requires: kernel-$flavor-devel = %version-%release$nl"
-	    head="${head}%endif$nl"
-	fi
-    done
-    requires="${head//$'\n'/\\n}"
-}
-
-# The pre-configured kernel source package
-if test -e $build_dir/kernel-default.spec; then
-    # if there is no kernel-default, assume that this is a "special"
-    # branch (such as slert) with it's own kernel-source
-    echo "kernel-source.spec"
-    prepare_source_and_syms kernel-syms # compute archs and requires
-    sed -e "s,@NAME@,kernel-source,g" \
-        -e "s,@VARIANT@,$VARIANT,g" \
-        -e "s,@SRCVERSION@,$SRCVERSION,g" \
-        -e "s,@PATCHVERSION@,$PATCHVERSION,g" \
-        -e "s,@RPMVERSION@,$RPMVERSION,g" \
-        -e "s,@BINARY_SPEC_FILES@,$binary_spec_files,g" \
-        -e "s,@TOLERATE_UNKNOWN_NEW_CONFIG_OPTIONS@,$tolerate_unknown_new_config_options," \
-        -e "s,@RELEASE@,$RELEASE,g" \
-      < rpm/kernel-source.spec.in \
-    > $build_dir/kernel-source.spec
-    install_changes $build_dir/kernel-source.changes
-
-    echo "kernel-syms.spec"
-    sed -e "s,@NAME@,kernel-syms,g" \
-        -e "s,@VARIANT@,,g" \
-        -e "s,@SRCVERSION@,$SRCVERSION,g" \
-        -e "s,@PATCHVERSION@,$PATCHVERSION,g" \
-        -e "s,@RPMVERSION@,$RPMVERSION,g" \
-        -e "s,@REQUIRES@,$requires,g" \
-        -e "s,@RELEASE@,$RELEASE,g" \
-      < rpm/kernel-syms.spec.in \
-    > $build_dir/kernel-syms.spec
-    install_changes $build_dir/kernel-syms.changes
-
-    install -m 644                              \
-        rpm/kernel-source.rpmlintrc             \
-        $build_dir/kernel-source.rpmlintrc
-fi
-
-if test -e $build_dir/kernel-rt.spec; then
-    echo "kernel-source-rt.spec"
-    # workaround
-    binary_spec_files=${binary_spec_files/kernel-syms.spec/kernel-syms-rt.spec}
-    prepare_source_and_syms kernel-syms-rt # compute archs and requires
-    sed -e "s,@NAME@,kernel-source-rt,g" \
-        -e "s,@VARIANT@,$VARIANT,g" \
-        -e "s,@SRCVERSION@,$SRCVERSION,g" \
-        -e "s,@PATCHVERSION@,$PATCHVERSION,g" \
-        -e "s,@RPMVERSION@,$RPMVERSION,g" \
-        -e "s,@BINARY_SPEC_FILES@,$binary_spec_files,g" \
-        -e "s,@TOLERATE_UNKNOWN_NEW_CONFIG_OPTIONS@,$tolerate_unknown_new_config_options," \
-        -e "s,@RELEASE@,$RELEASE,g" \
-      < rpm/kernel-source.spec.in \
-    > $build_dir/kernel-source-rt.spec
-    install_changes $build_dir/kernel-source-rt.changes
-
-    echo "kernel-syms-rt.spec"
-    sed -e "s,@NAME@,kernel-syms-rt,g" \
-        -e "s,@VARIANT@,-rt,g" \
-        -e "s,@SRCVERSION@,$SRCVERSION,g" \
-        -e "s,@PATCHVERSION@,$PATCHVERSION,g" \
-        -e "s,@RPMVERSION@,$RPMVERSION,g" \
-        -e "s,@REQUIRES@,$requires,g" \
-        -e "s,@RELEASE@,$RELEASE,g" \
-      < rpm/kernel-syms.spec.in \
-    > $build_dir/kernel-syms-rt.spec
-    install_changes $build_dir/kernel-syms-rt.changes
-
-    install -m 644                              \
-        rpm/kernel-source.rpmlintrc             \
-        $build_dir/kernel-source-rt.rpmlintrc
-fi
 
 echo "Copying various files..."
 install -m 644					\
 	config.conf				\
 	supported.conf				\
-	rpm/source-pre.sh			\
-	rpm/source-post.sh			\
-	rpm/pre.sh				\
-	rpm/post.sh				\
-	rpm/preun.sh				\
-	rpm/postun.sh				\
+	rpm/config.sh                           \
+	rpm/old-packages.conf                   \
+	rpm/kernel-{binary,source,syms}.spec.in \
+	rpm/source-{pre,post}.sh		\
+	rpm/{pre,post,preun,postun}.sh		\
 	rpm/module-renames			\
 	rpm/kernel-module-subpackage		\
 	rpm/macros.kernel-source		\
@@ -420,17 +134,8 @@ install -m 644					\
 	doc/README.KSYMS			\
 	$build_dir
 
-
-if [ -x /work/src/bin/tools/convert_changes_to_rpm_changelog ]; then
-    /work/src/bin/tools/convert_changes_to_rpm_changelog kernel-source.changes \
-	kernel-source.changes.old >"$build_dir"/rpm_changelog
-    for spec in "$build_dir"/*.spec; do
-        (echo "%changelog"; cat "$build_dir"/rpm_changelog) >>"$spec"
-    done
-    rm -f "$build_dir"/rpm_changelog
-fi
-
 install -m 755					\
+	rpm/mkspec                              \
 	rpm/find-provides			\
 	rpm/config-subst 			\
 	rpm/check-for-config-changes		\
@@ -442,16 +147,17 @@ install -m 755					\
 	scripts/guards				\
 	scripts/arch-symbols			\
 	misc/extract-modaliases			\
+	rpm/compute-PATCHVERSION.sh             \
 	$build_dir
+
+cat kernel-source.changes{,.old} > "$build_dir/kernel-source$VARIANT.changes"
+install -m 644 rpm/kernel-source.rpmlintrc \
+	"$build_dir/kernel-source$VARIANT.rpmlintrc"
 
 if [ -e extra-symbols ]; then
 	install -m 755					\
 		extra-symbols				\
 		$build_dir
-fi
-
-if [ -n "$update_only" ]; then
-	exit 0
 fi
 
 if [ -r $SRC_FILE ]; then
@@ -546,7 +252,7 @@ stable_tar $build_dir/kabi.tar.bz2 kabi
 # not already created: patches.addon is empty by intention; others currently
 # may contain no patches.
 archives=$(sed -ne 's,^Source[0-9]*:.*[ \t/]\([^/]*\)\.tar\.bz2$,\1,p' \
-           $build_dir/*.spec | sort -u)
+           $build_dir/kernel-binary.spec.in | sort -u)
 for archive in $archives; do
     [ "$archive" = "linux-%srcversion" ] && continue
     if ! [ -e $build_dir/$archive.tar.bz2 ]; then
@@ -566,8 +272,12 @@ echo $((1024*1024)) > $build_dir/minmem
 # Force mbuild to choose build hosts with enough disk space available:
 echo $((6*1024)) > $build_dir/needed_space_in_mb
 if [ -n "$ignore_kabi" ]; then
-    ( for i in `seq 0 42` ; do echo "This file named IGNORE-KABI-BADNESS is not empty." ; done ) > $build_dir/IGNORE-KABI-BADNESS
+    echo > $build_dir/IGNORE-KABI-BADNESS
 fi
 if [ -n "$ignore_unsupported_deps" ]; then
-    touch $build_dir/IGNORE-UNSUPPORTED-DEPS
+    echo > $build_dir/IGNORE-UNSUPPORTED-DEPS
 fi
+
+echo "cd $build_dir; ./mkspec"
+cd "$build_dir"
+./mkspec
