7cf8cce123198c73d2d575a418c47f0594958ee3
Michal Marek <mmarek@suse.cz>
2009-12-15 12:50:58 +0100

    - rpm/config.sh: set OBS_PROJECT and IBS_PROJECT variables for   osc_wrapper.

--- rpm/config.sh
+++ rpm/config.sh
@@ -2,3 +2,6 @@
 SRCVERSION=2.6.32
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
+# buildservice projects to build the kernel against
+OBS_PROJECT=openSUSE:Factory
+IBS_PROJECT=SUSE:Factory:Head
