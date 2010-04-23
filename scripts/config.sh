17633f5510d8593e15117bb107ea340fb4ca309e
Michal Marek <mmarek@suse.cz>
2010-04-23 15:58:37 +0200

    scripts/config.sh: Fall back to rpm/config.sh if dirname didn't work

--- scripts/config.sh
+++ scripts/config.sh
@@ -5,6 +5,8 @@ fi
 # SRCVERSION and VARIANT are set in rpm/config.sh
 if test -e $(dirname -- "$0")/../rpm/config.sh; then
     source "$_"
+elif test -e rpm/config.sh; then
+    source "$_"
 fi
 BUILD_DIR=kernel-source${VARIANT}
 IGNORE_ARCHS=
