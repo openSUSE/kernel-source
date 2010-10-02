8a78f00a7da312034e74b255ff4eb6bff0fbc69d
Michal Marek <mmarek@suse.cz>
2010-10-03 00:01:53 +0200

    Automatically updated to 2.6.36-rc6-84-gc6ea21e

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=2.6.36-rc6-76-g18ffe4b
+SRCVERSION=2.6.36-rc6-84-gc6ea21e
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
