41a681bda6188516b66c7f76ca32724298ece7a9
Michal Marek <mmarek@suse.cz>
2010-12-02 00:01:32 +0100

    Automatically updated to 2.6.37-rc4-12-g22a5b56

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=2.6.37-rc4
+SRCVERSION=2.6.37-rc4-12-g22a5b56
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
