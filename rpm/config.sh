bf19860f0f52aab557d756fa258000e7fb09717e
Michal Marek <mmarek@suse.cz>
2010-12-09 14:48:13 +0100

    - rpm/config.sh: Build against openSUSE:Factory

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,5 +3,5 @@ SRCVERSION=2.6.36
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=openSUSE:11.3
+OBS_PROJECT=openSUSE:Factory
 IBS_PROJECT=SUSE:Factory:Head
