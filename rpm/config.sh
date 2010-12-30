8da14386865c7690a7c7a031a44570a49e9f8e8b
Michal Marek <mmarek@suse.cz>
2010-12-31 00:01:44 +0100

    Automatically updated to 2.6.37-rc8-14-g3643e0e

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=2.6.37-rc8
+SRCVERSION=2.6.37-rc8-14-g3643e0e
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
