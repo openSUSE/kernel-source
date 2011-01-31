fa2448ac0839b75190a2851f8885025d54f890ad
Michal Marek <mmarek@suse.cz>
2011-01-31 08:02:17 +0100

    Automatically updated to 2.6.38-rc2-324-g70d1f36

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=2.6.38-rc2-274-g1f0324c
+SRCVERSION=2.6.38-rc2-324-g70d1f36
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
