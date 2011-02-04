d028aa320617a78d6cb0df393bf97f7bc100d068
Michal Marek <mmarek@suse.cz>
2011-02-04 22:22:44 +0100

    - rpm/config.sh, scripts/wd-functions.sh: Change the SRCVERSION format   for linux-next to 2.6.<version>-next-<date> - rpm/compute-PATCHVERSION.sh: Fix sed regexp

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=next-20110204
+SRCVERSION=2.6.38-rc3-next-20110204
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
