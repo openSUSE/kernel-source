f945d3ad2a8935b090e79db80019f10b99bcf2f0
Michal Marek <mmarek@suse.cz>
2011-02-07 18:20:11 +0100

    - rpm/{config.sh,kernel-source.spec.in,mkspec}: Generate only   kernel-source-vanilla in the linux-next and vanilla branches.

--- rpm/config.sh
+++ rpm/config.sh
@@ -2,6 +2,8 @@
 SRCVERSION=2.6.38-rc4
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
+# Set to 1 if the tree does not contain any non-vanilla patches at all
+VANILLA_ONLY=1
 # buildservice projects to build the kernel against
 OBS_PROJECT=openSUSE:11.3
 IBS_PROJECT=SUSE:Factory:Head
