40cd327eac0fc978a2cb279fe489908271a3394b
Michal Marek <mmarek@suse.cz>
2011-03-23 08:02:02 +0100

    Automatically updated to 2.6.38-7035-g6447f55d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=2.6.38-6671-geddecbb
+SRCVERSION=2.6.38-7035-g6447f55d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
