dc17050a0e212f2b47021456e98d22d3c0b2fbd9
Michal Marek <mmarek@suse.cz>
2011-04-18 08:02:01 +0200

    Automatically updated to 2.6.39-rc3-240-ga1b49cb

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=2.6.39-rc3-228-gd733ed6
+SRCVERSION=2.6.39-rc3-240-ga1b49cb
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
