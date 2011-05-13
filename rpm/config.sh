a8a170fb9019df7ee6945ab95fda3a28fca74feb
Jeff Mahoney <jeffm@suse.com>
2011-05-13 09:45:04 -0400

    - Update to 2.6.39-rc7.   - Eliminated 5 patches.

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=2.6.39-rc6
+SRCVERSION=2.6.39-rc7
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
