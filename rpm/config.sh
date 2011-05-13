5493cb8fb91ab4331cb91626b6332303bfaa1cdd
Michal Marek <mmarek@suse.cz>
2011-05-13 08:01:56 +0200

    Automatically updated to 2.6.39-rc7-146-g446cc63

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=2.6.39-rc7-104-g3568bd9
+SRCVERSION=2.6.39-rc7-146-g446cc63
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
