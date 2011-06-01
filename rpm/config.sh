da915a8a4de6de082ca19ab847e65524c56d8d30
Michal Marek <mmarek@suse.cz>
2011-06-01 08:02:10 +0200

    Automatically updated to 3.0.0-rc1-next-20110601

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.0.0-rc1-next-20110530
+SRCVERSION=3.0.0-rc1-next-20110601
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
