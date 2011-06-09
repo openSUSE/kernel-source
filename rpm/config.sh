0eba01fe3ec66a34959d111c7df2163abead39e8
Michal Marek <mmarek@suse.cz>
2011-06-09 08:02:09 +0200

    Automatically updated to 3.0.0-rc2-196-g06e8684

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.0.0-rc2-108-gcb0a02e
+SRCVERSION=3.0.0-rc2-196-g06e8684
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
