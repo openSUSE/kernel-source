5ebc378ca169caad12efa22a14203cf708474604
Michal Marek <mmarek@suse.cz>
2011-06-15 08:02:50 +0200

    Automatically updated to 3.0-rc3-55-gada9c93

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.0-rc3
+SRCVERSION=3.0-rc3-55-gada9c93
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
