bb3c705abd85192ebf27fd46c6d9c055f076a67e
Michal Marek <mmarek@suse.cz>
2011-07-25 08:04:10 +0200

    Automatically updated to 3.0-3370-gb6844e8

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.0-2871-g6d6be43
+SRCVERSION=3.0-3370-gb6844e8
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
