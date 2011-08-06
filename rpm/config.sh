02ebaf2e040dc8fc02a341c0271849e5152e4b05
Michal Marek <mmarek@suse.cz>
2011-08-06 08:00:56 +0200

    Automatically updated to 3.0-7530-gde96355

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.0-7372-g53d1e65
+SRCVERSION=3.0-7530-gde96355
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
