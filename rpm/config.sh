ad60c51d7d6b25e502cf98e2d5837252474a26df
Michal Marek <mmarek@suse.cz>
2011-08-19 08:01:55 +0200

    Automatically updated to 3.1-rc2-122-g5c80c71

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.1-rc2-91-g338d0f0
+SRCVERSION=3.1-rc2-122-g5c80c71
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
