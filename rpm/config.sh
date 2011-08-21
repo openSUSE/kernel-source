783f4ae653d9a873bc2cee250d036197e2d70063
Michal Marek <mmarek@suse.cz>
2011-08-21 08:01:44 +0200

    Automatically updated to 3.1-rc2-171-gbed8cad

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.1-rc2-169-g5ccc387
+SRCVERSION=3.1-rc2-171-gbed8cad
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
