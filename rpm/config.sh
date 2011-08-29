0b09889f31aa2a49f29c29ebfc67e1c03d1bb628
Michal Marek <mmarek@suse.cz>
2011-08-29 14:06:14 +0200

    Automatically updated to 3.1-rc3-next-20110829

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.1-rc3-next-20110823
+SRCVERSION=3.1-rc3-next-20110829
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
