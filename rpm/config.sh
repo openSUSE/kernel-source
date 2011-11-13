40459535f88398e999f9efcf1581c8e923e4b2af
Michal Marek <mmarek@suse.cz>
2011-11-13 08:00:32 +0100

    Automatically updated to 3.2-rc1-282-g3439a8d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.2-rc1-252-g8f042aa
+SRCVERSION=3.2-rc1-282-g3439a8d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
