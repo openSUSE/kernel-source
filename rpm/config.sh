f3eab4932d27f69406cebf2b78d0e9412de47e8e
Michal Marek <mmarek@suse.cz>
2011-11-29 08:01:41 +0100

    Automatically updated to 3.2-rc3-94-g8748dfa

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.2-rc3-23-g401d006
+SRCVERSION=3.2-rc3-94-g8748dfa
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
