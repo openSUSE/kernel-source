310e0789871de78206e8a5d92d1d7e8b91c1d21d
Michal Marek <mmarek@suse.cz>
2011-12-02 08:00:28 +0100

    Automatically updated to 3.2-rc4-77-g5983fe2

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.2-rc3-187-g11d814a
+SRCVERSION=3.2-rc4-77-g5983fe2
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
