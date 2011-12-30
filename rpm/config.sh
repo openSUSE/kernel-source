9f9662f2ffdeb1040b705d01a024022e668490d2
Michal Marek <mmarek@suse.cz>
2011-12-30 08:00:49 +0100

    Automatically updated to 3.2-rc7-39-g89307ba

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.2-rc7-17-g371de6e
+SRCVERSION=3.2-rc7-39-g89307ba
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
