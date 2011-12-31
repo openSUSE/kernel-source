c548d071587a2545a57f3d5b938d501ad3880a6c
Michal Marek <mmarek@suse.cz>
2011-12-31 08:01:04 +0100

    Automatically updated to 3.2-rc7-72-g06867fb

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.2-rc7-39-g89307ba
+SRCVERSION=3.2-rc7-72-g06867fb
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
