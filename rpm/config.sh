ad91484062245dd4009ffd6392239d2ba8d9a622
Michal Marek <mmarek@suse.cz>
2012-01-11 08:02:45 +0100

    Automatically updated to 3.2-6100-ge343a89

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.2-4874-ge4e1118
+SRCVERSION=3.2-6100-ge343a89
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
