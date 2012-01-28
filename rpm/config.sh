3320c4a946ef4987a950dcaf607f346c303166c0
Michal Marek <mmarek@suse.cz>
2012-01-28 08:01:47 +0100

    Automatically updated to 3.3-rc1-287-gacb42a3

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.3-rc1-246-g74ea15d
+SRCVERSION=3.3-rc1-287-gacb42a3
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
