3d37f5a7903cc12cacfb74cffc284f0209573989
Michal Marek <mmarek@suse.cz>
2012-02-14 08:02:34 +0100

    Automatically updated to 3.3-rc3-next-20120214

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.3-rc3-next-20120213
+SRCVERSION=3.3-rc3-next-20120214
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
