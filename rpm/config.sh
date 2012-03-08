a9ed95b5ff650f4a3484b6c39c50f8251970ae36
Michal Marek <mmarek@suse.cz>
2012-03-08 08:02:11 +0100

    Automatically updated to 3.3-rc6-164-g4f262ac

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.3-rc6-146-gdac12d1
+SRCVERSION=3.3-rc6-164-g4f262ac
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
