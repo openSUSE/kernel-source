4b461b556a5db1b701ea4bb634a5a0759fe0b04c
Michal Marek <mmarek@suse.cz>
2012-04-30 13:04:31 +0200

    Automatically updated to 3.4-rc5-next-20120430

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.4-rc4-next-20120424
+SRCVERSION=3.4-rc5-next-20120430
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
