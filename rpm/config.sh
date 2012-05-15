2748f808283066628181969628ab08e18900d1e1
Michal Marek <mmarek@suse.cz>
2012-05-15 08:00:38 +0200

    Automatically updated to 3.4-rc7-21-gb6255ee

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.4-rc7-4-g9ff00d5
+SRCVERSION=3.4-rc7-21-gb6255ee
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
