3c2218a9b94f284189b8cdd23b6174e81bd16f14
Michal Marek <mmarek@suse.cz>
2012-05-27 08:00:25 +0200

    Automatically updated to 3.4-8215-g1e2aec8

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.4-7822-g786f02b
+SRCVERSION=3.4-8215-g1e2aec8
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
