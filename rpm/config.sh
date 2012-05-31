7703b4d647b204d5345e241691ab2dbfe74b00eb
Michal Marek <mmarek@suse.cz>
2012-05-31 08:00:35 +0200

    Automatically updated to 3.4-9208-gaf56e0a

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.4-8906-g731a737
+SRCVERSION=3.4-9208-gaf56e0a
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
