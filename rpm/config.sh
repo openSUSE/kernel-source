d6982b6608da14ccef12bdd0577e1134d540b23d
Michal Marek <mmarek@suse.cz>
2012-06-15 08:00:35 +0200

    Automatically updated to 3.5-rc2-172-g424d54d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.5-rc2-58-g790b9d4
+SRCVERSION=3.5-rc2-172-g424d54d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
