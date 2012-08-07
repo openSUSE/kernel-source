4099be60b25ddb64b28d001dad6924f422e39f0c
Michal Marek <mmarek@suse.cz>
2012-08-07 17:32:53 +0200

    rpm/config.sh: Build the KOTD against 12.2

--- rpm/config.sh
+++ rpm/config.sh
@@ -5,5 +5,5 @@ VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
 VANILLA_ONLY=1
 # buildservice projects to build the kernel against
-OBS_PROJECT=openSUSE:12.1
-IBS_PROJECT=SUSE:Factory:Head
+OBS_PROJECT=openSUSE:12.2:Update
+IBS_PROJECT=
