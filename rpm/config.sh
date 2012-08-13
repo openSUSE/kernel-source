fc2ed37fe5b8ea4c071d1c58cc78aabc037e782e
Michal Marek <mmarek@suse.cz>
2012-08-13 08:01:49 +0200

    Automatically updated to 3.6-rc1-270-gf82202b

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.6-rc1-207-gf4ba394
+SRCVERSION=3.6-rc1-270-gf82202b
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
