722b617382816f8e231073c3951e33207206d8df
Michal Marek <mmarek@suse.cz>
2012-09-11 09:45:11 +0200

    Automatically updated to 3.6-rc5-next-20120911

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.6-rc4-next-20120907
+SRCVERSION=3.6-rc5-next-20120911
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
