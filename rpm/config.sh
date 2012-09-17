d6432fe4fe094806103c2c681ef91fe75bbbd9ff
Jeff Mahoney <jeffm@suse.com>
2012-09-17 09:57:42 -0400

    Update to 3.6-rc6. - Eliminated 1 patch.
    patches.arch/arm-phys-offset.patch needs review.

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.6-rc5
+SRCVERSION=3.6-rc6
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
