2ed828b5d1b8cefa37f7f2e3ca8ed5fc0989e2d1
Michal Marek <mmarek@suse.cz>
2012-09-20 10:55:23 +0200

    Automatically updated to 3.6-rc6-52-gc46de22

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.6-rc6-28-g925a6f0
+SRCVERSION=3.6-rc6-52-gc46de22
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
