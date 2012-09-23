8811f160184dcdc8a74fc1e94277a7cf77cadff4
Michal Marek <mmarek@suse.cz>
2012-09-23 08:00:28 +0200

    Automatically updated to 3.6-rc6-193-ge5e77cf

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.6-rc6-176-gabef3bd
+SRCVERSION=3.6-rc6-193-ge5e77cf
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
