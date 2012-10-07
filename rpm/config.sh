64a3d263d6cb733b2886f6950334c89f11527d8d
Michal Marek <mmarek@suse.cz>
2012-10-07 08:00:27 +0200

    Automatically updated to 3.6-7201-ged5062d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.6-7124-g5f3d2f2
+SRCVERSION=3.6-7201-ged5062d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
