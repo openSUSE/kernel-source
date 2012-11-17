46a9f26873764a3bc2dad7e82be54b24377baf91
Michal Marek <mmarek@suse.cz>
2012-11-17 08:01:15 +0100

    Automatically updated to 3.7-rc6

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.7-rc5-68-gc5e35d6
+SRCVERSION=3.7-rc6
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
