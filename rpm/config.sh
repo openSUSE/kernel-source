d760411f5031f6b71b50d296211f7b9a0b69387e
Jiri Slaby <jslaby@suse.cz>
2012-11-18 13:04:10 +0100

    rpm/config.sh: build against 12.2
    It should fix the firmware build problem.

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,5 +3,5 @@ SRCVERSION=3.6
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=openSUSE:12.1:Update
+OBS_PROJECT=openSUSE:12.2:Update
 IBS_PROJECT=
