03f6291bfa7ed97ab2f2b7b18f67af1d4301a749
Michal Marek <mmarek@suse.cz>
2012-11-29 08:02:47 +0100

    Automatically updated to 3.7-rc7-71-ge9296e8

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.7-rc7-51-ge23739b
+SRCVERSION=3.7-rc7-71-ge9296e8
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
