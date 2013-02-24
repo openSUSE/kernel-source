f2dc199be6f57b45a4a6531236c2eecf472034a6
Michal Marek <mmarek@suse.cz>
2013-02-24 08:02:05 +0100

    Automatically updated to 3.8-6988-g9e2d59a

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.8-6543-gdf24eef
+SRCVERSION=3.8-6988-g9e2d59a
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
