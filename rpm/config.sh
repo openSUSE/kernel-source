20fc41c307ac0c122ff466c7c858f326050e0dee
Jiri Slaby <jslaby@suse.cz>
2013-03-23 00:08:34 +0100

    rpm/config.sh: build against 12.3

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,7 +3,7 @@ SRCVERSION=3.8
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=openSUSE:12.2:Update
+OBS_PROJECT=openSUSE:12.3:Update
 OBS_PROJECT_ARM=openSUSE:12.2:ARM
 IBS_PROJECT=
 IBS_PROJECT_ARM=Devel:ARM:12.3
