e38a9e947b8fa75a768163659e3d79634f871392
Michal Marek <mmarek@suse.cz>
2013-04-11 08:01:42 +0200

    Automatically updated to 3.9-rc6-115-g7ee32a6

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.9-rc6-43-gd02a9a8
+SRCVERSION=3.9-rc6-115-g7ee32a6
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
