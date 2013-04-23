1253c1af3b47416924df24c638fa23d76dccb8da
Michal Marek <mmarek@suse.cz>
2013-04-23 12:02:07 +0200

    Automatically updated to 3.9-rc8-5-g824282c

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.9-rc8
+SRCVERSION=3.9-rc8-5-g824282c
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
