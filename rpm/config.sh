6fc0bd5c1cf540e8662c266433ab651f040b9584
Michal Marek <mmarek@suse.cz>
2013-05-04 00:56:33 +0200

    rpm/config.sh: Drop the ARM repository in Kernel:HEAD
    Use the "ports" repository of openSUSE:Factory instead.

--- rpm/config.sh
+++ rpm/config.sh
@@ -4,6 +4,5 @@ SRCVERSION=3.9
 VARIANT=
 # buildservice projects to build the kernel against
 OBS_PROJECT=openSUSE:Factory
-OBS_PROJECT_ARM=openSUSE:Factory:ARM
 IBS_PROJECT=SUSE:Factory:Head
 IBS_PROJECT_ARM=Devel:ARM:Factory
