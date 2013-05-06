deee5a936425d336c37321e8972054fb20f1009c
Michal Marek <mmarek@suse.cz>
2013-05-06 12:01:06 +0200

    Automatically updated to 3.9-10518-gd7ab730

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.9-10104-g1aaf6d3
+SRCVERSION=3.9-10518-gd7ab730
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
