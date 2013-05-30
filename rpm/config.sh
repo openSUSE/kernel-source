0e54677261214ce91999ecc9b9e5ac167fed16bc
Michal Marek <mmarek@suse.cz>
2013-05-30 12:01:56 +0200

    Automatically updated to 3.10-rc3-68-gdcdbe33

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.10-rc3-46-g58f8bbd
+SRCVERSION=3.10-rc3-68-gdcdbe33
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
