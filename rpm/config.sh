68f68a0d7dcadb5e88bd928996ac652cba02a7d6
Michal Marek <mmarek@suse.cz>
2013-08-03 12:01:57 +0200

    Automatically updated to 3.11-rc3-288-gabe0308

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.11-rc3-207-g64ccccf
+SRCVERSION=3.11-rc3-288-gabe0308
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
