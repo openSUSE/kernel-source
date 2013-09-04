97de23f10d2a74b2111549ad12e281b9708b4d06
Michal Marek <mmarek@suse.cz>
2013-09-04 12:01:55 +0200

    Automatically updated to 3.11-2654-g458c3f6

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.11
+SRCVERSION=3.11-2654-g458c3f6
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
