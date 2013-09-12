5f2bc8421a1447d63919b5c5291a8e23f59595c1
Michal Marek <mmarek@suse.cz>
2013-09-12 12:00:33 +0200

    Automatically updated to 3.11-9420-gd5d04bb

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.11-9031-ga22a0fd
+SRCVERSION=3.11-9420-gd5d04bb
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
