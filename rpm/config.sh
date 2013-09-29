edc76c0736f6879c594a9506b5b4f1208d7557d4
Michal Marek <mmarek@suse.cz>
2013-09-29 12:00:32 +0200

    Automatically updated to 3.12-rc2-160-g669fc2f

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.12-rc2-108-g6cac446
+SRCVERSION=3.12-rc2-160-g669fc2f
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
