4a91cc829e00caa6966aecad0b637caf2ace76f2
Michal Marek <mmarek@suse.cz>
2013-11-05 12:01:05 +0100

    Automatically updated to 3.12-48-gbe408cd

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.12
+SRCVERSION=3.12-48-gbe408cd
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
