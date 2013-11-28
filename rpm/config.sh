82bd906d7ae69f3eb0d382d5f5b2d380650b8ec4
Michal Marek <mmarek@suse.cz>
2013-11-28 16:36:22 +0100

    Automatically updated to 3.13-rc1-229-ge7c0d3d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.13-rc1-141-g8ae516a
+SRCVERSION=3.13-rc1-229-ge7c0d3d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
