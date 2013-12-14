736154eb7b85991628163c3223e83b126229d0a3
Michal Marek <mmarek@suse.cz>
2013-12-14 12:02:27 +0100

    Automatically updated to 3.13-rc3-362-gb2077eb

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.13-rc3-302-g8d27637
+SRCVERSION=3.13-rc3-362-gb2077eb
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
