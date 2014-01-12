a0e708376c7dfccc9846478a123b7a5db0e91060
Michal Marek <mmarek@suse.cz>
2014-01-12 12:04:50 +0100

    Automatically updated to 3.13-rc8

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.13-rc7-126-g228fdc0
+SRCVERSION=3.13-rc8
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
