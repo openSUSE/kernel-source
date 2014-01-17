a983897664e13f8311b1dbe70f5dca75facc9bfe
Michal Marek <mmarek@suse.cz>
2014-01-17 11:35:12 +0100

    Revert "rpm/config.sh: Drop the ARM and PPC repositories"
    This was actually wrong, the ports repository is broken.

--- rpm/config.sh
+++ rpm/config.sh
@@ -4,4 +4,7 @@ SRCVERSION=3.13-rc8
 VARIANT=
 # buildservice projects to build the kernel against
 OBS_PROJECT=openSUSE:Factory
+OBS_PROJECT_ARM=openSUSE:Factory:ARM
+OBS_PROJECT_PPC=openSUSE:Factory:PowerPC
 IBS_PROJECT=SUSE:Factory:Head
+IBS_PROJECT_ARM=Devel:ARM:Factory
