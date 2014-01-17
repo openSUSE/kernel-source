96b9cec2ea2912b2a794212275a99c7a61bddd9c
Michal Marek <mmarek@suse.cz>
2014-01-17 11:31:25 +0100

    rpm/config.sh: Drop the ARM and PPC repositories
    The architectures are provided by the ports repository

--- rpm/config.sh
+++ rpm/config.sh
@@ -4,7 +4,4 @@ SRCVERSION=3.13-rc8
 VARIANT=
 # buildservice projects to build the kernel against
 OBS_PROJECT=openSUSE:Factory
-OBS_PROJECT_ARM=openSUSE:Factory:ARM
-OBS_PROJECT_PPC=openSUSE:Factory:PowerPC
 IBS_PROJECT=SUSE:Factory:Head
-IBS_PROJECT_ARM=Devel:ARM:Factory
