dace9fb95b91b2bc5de9fa42cbe58b7d1eeaa533
Michal Marek <mmarek@suse.cz>
2014-02-11 12:02:06 +0100

    Automatically updated to 3.14-rc2-26-g6792dfe

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.14-rc2
+SRCVERSION=3.14-rc2-26-g6792dfe
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
