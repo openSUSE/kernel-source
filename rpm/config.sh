39313ba4ede5e081f38a4d7aef762fec69d40f35
Michal Marek <mmarek@suse.cz>
2014-03-08 12:02:11 +0100

    Automatically updated to 3.14-rc5-265-gb01d4e6

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.14-rc5-185-gc3bebc7
+SRCVERSION=3.14-rc5-265-gb01d4e6
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
