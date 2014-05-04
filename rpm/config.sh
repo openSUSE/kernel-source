de6d8ef3f3d60cda9601788be6dbfd5dbb8a796b
Michal Marek <mmarek@suse.cz>
2014-05-04 12:03:11 +0200

    Automatically updated to 3.15-rc3-177-g0384dca

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.15-rc3-159-g6c6ca9c
+SRCVERSION=3.15-rc3-177-g0384dca
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
