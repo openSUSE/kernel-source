7987b82f4b858fa6b7b68b8eb64cc0d7d69d3a05
Michal Marek <mmarek@suse.cz>
2014-06-04 12:02:44 +0200

    Automatically updated to 3.15-rc8-58-gd2cfd31

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.15-rc8-53-gcae61ba
+SRCVERSION=3.15-rc8-58-gd2cfd31
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
