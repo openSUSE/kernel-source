20e30d3cd2e8e52001a103f4d750a452df41cc2e
Michal Marek <mmarek@suse.cz>
2014-08-01 12:09:00 +0200

    Automatically updated to 3.16-rc7-next-20140801

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.16-rc7-next-20140730
+SRCVERSION=3.16-rc7-next-20140801
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
