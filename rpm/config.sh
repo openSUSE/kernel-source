0907820f60d4d297ffdc652e6b2b0749c80ba181
Michal Marek <mmarek@suse.cz>
2014-08-04 12:03:18 +0200

    Automatically updated to 3.16

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.16-rc7-111-g3f9c08f
+SRCVERSION=3.16
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
