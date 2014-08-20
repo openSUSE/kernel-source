e56149824579ea8ba189adf95d6064d53ac89ca5
Michal Marek <mmarek@suse.cz>
2014-08-20 17:36:52 +0200

    rpm/compress-vmlinux.sh: Do not duplicate the job of brp-99-compress-vmlinux

--- rpm/compress-vmlinux.sh
+++ rpm/compress-vmlinux.sh
@@ -2,6 +2,11 @@
 
 set -x
 
+# Once the brp script is reasonably widespread, we will remove the specfile
+# hack and this script
+if test -x /usr/lib/rpm/brp-suse.d/brp-99-compress-vmlinux; then
+	exit 0
+fi
 vmlinux=$1
 if test -e "$vmlinux" -a -e "$vmlinux.gz"; then
 	# Deliberately not using gzip -n; the vmlinux image has a predictable
