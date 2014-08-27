90bc0f1c2238b9aec5a78d9c76dc9663e8fa0316
Michal Marek <mmarek@suse.cz>
2014-08-27 16:45:38 +0200

    rpm/config.sh Use the same repository setup as the master branch

--- rpm/config.sh
+++ rpm/config.sh
@@ -4,4 +4,7 @@ SRCVERSION=3.16
 VARIANT=
 # buildservice projects to build the kernel against
 OBS_PROJECT=openSUSE:Factory
-IBS_PROJECT=
+OBS_PROJECT_ARM=openSUSE:Factory:ARM
+OBS_PROJECT_PPC=openSUSE:Factory:PowerPC
+IBS_PROJECT=SUSE:Factory:Head
+IBS_PROJECT_ARM=Devel:ARM:Factory
