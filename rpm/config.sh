9eab6aee64559ccffa3f944fd84e83e982ded54c
Michal Marek <mmarek@suse.cz>
2014-09-14 12:02:00 +0200

    Automatically updated to 3.17-rc4-355-g1536340

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.17-rc4-337-gfc486b0
+SRCVERSION=3.17-rc4-355-g1536340
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
