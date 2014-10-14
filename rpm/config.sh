3f870b67a9758fca6965707f9ef056ff28632c46
Michal Marek <mmarek@suse.cz>
2014-10-14 12:02:15 +0200

    Automatically updated to 3.17-9283-g2d65a9f

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.17-7747-g5e40d33
+SRCVERSION=3.17-9283-g2d65a9f
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
