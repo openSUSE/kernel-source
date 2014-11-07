a2308cf063165772463d1d0eb71aee4bffe73253
Michal Marek <mmarek@suse.cz>
2014-11-07 12:02:09 +0100

    Automatically updated to 3.18-rc3-82-ged78bb8

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.18-rc3-68-g20f3963
+SRCVERSION=3.18-rc3-82-ged78bb8
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
