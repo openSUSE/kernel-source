7869507f8334eeb09ceca9a77b9c3b5e54524e46
Michal Marek <mmarek@suse.cz>
2014-12-18 12:01:55 +0100

    Automatically updated to 3.18-11290-g44e8967

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.18-11052-g603ba7e
+SRCVERSION=3.18-11290-g44e8967
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
