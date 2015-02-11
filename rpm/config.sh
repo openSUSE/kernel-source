b382c375d36387120d85178d3266ed2a0bdf82fa
Michal Marek <mmarek@suse.cz>
2015-02-11 12:03:03 +0100

    Automatically updated to 3.19-2595-gc5ce28d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.19-463-g3e8c04e
+SRCVERSION=3.19-2595-gc5ce28d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
