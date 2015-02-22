b14f6cbd42b34653db7cc57efd7b8c89b23829a7
Michal Marek <mmarek@suse.cz>
2015-02-22 12:01:47 +0100

    Automatically updated to 3.19-9526-ga135c71

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=3.19-8996-g4fbd0a8
+SRCVERSION=3.19-9526-ga135c71
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
