c54583f2ffc7f9689191276e4aa228e32e69d961
Michal Marek <mmarek@suse.cz>
2015-03-10 13:53:39 +0100

    Automatically updated to 4.0-rc3-next-20150310

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.0-rc2-next-20150306
+SRCVERSION=4.0-rc3-next-20150310
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
