6e7df207afe5742fcfc8e5809375a062ccc80283
Michal Marek <mmarek@suse.cz>
2015-03-12 12:02:00 +0100

    Automatically updated to 4.0-rc3-122-gcca28a5

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.0-rc3-111-gaffb817
+SRCVERSION=4.0-rc3-122-gcca28a5
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
