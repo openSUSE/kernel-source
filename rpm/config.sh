50f5e35a3131384c154e2209ea98e817695ec25e
Michal Marek <mmarek@suse.cz>
2015-04-04 12:02:11 +0200

    Automatically updated to 4.0-rc6-162-g1cced50

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.0-rc6-130-g8f778bb
+SRCVERSION=4.0-rc6-162-g1cced50
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
