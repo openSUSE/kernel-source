5294f69967acdb8e97f7f7f737565c42c0d78a96
Michal Marek <mmarek@suse.cz>
2015-08-10 15:46:30 +0200

    Automatically updated to 4.2-rc6-next-20150810

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.2-rc5-next-20150807
+SRCVERSION=4.2-rc6-next-20150810
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
