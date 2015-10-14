672b4cb6f67171d169b12479737098101aa045be
Kernel Build Daemon <kbuild@suse.de>
2015-10-14 12:02:35 +0200

    Automatically updated to 4.3-rc5-37-g5b5f145

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.3-rc5
+SRCVERSION=4.3-rc5-37-g5b5f145
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
