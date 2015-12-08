dd5041e12606bba909b0dadbcefab17e5bcf3a10
Michal Marek <mmarek@suse.com>
2015-12-08 15:54:01 +0100

    Automatically updated to 4.4-rc4-16-g62ea1ec5e17f

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.4-rc4
+SRCVERSION=4.4-rc4-16-g62ea1ec5e17f
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
