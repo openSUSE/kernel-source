030e995f86208c8a36fca1c096e5146e4a4ff388
Michal Marek <mmarek@suse.com>
2016-04-18 16:37:44 +0200

    Automatically updated to 4.6-rc3-next-20160418

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.6-rc2-next-20160404
+SRCVERSION=4.6-rc3-next-20160418
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
