ff60fc35db2290aa049dbfcadf016f6a0b9503a5
Kernel Build Daemon <kbuild@suse.de>
2016-05-19 12:00:43 +0200

    Automatically updated to 4.6-5028-g2600a46

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.6-3623-g0b7962a
+SRCVERSION=4.6-5028-g2600a46
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
