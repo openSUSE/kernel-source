5c307b1a392c8634364bb02fd331116d44abdcf6
Egbert Eich <eich@suse.com>
2016-05-20 16:48:21 +0200

    scripts/sequence-patch.sh: Fixed spelling of cscope

--- scripts/sequence-patch.sh
+++ scripts/sequence-patch.sh
@@ -587,7 +587,7 @@ if test "$SP_BUILD_DIR" != "$PATCH_DIR"; then
     ln -sf "$PATCH_DIR" "$SP_BUILD_DIR/source"
     ln -sf "source/patches" "$SP_BUILD_DIR/patches"
     cat > $PATCH_DIR/GNUmakefile <<EOF
-ifeq ($(filter tags TAGS cscsope gtags, $(MAKECMDGOALS)),)
+ifeq ($(filter tags TAGS cscope gtags, $(MAKECMDGOALS)),)
 ifndef KBUILD_OUTPUT
 KBUILD_OUTPUT=$SP_BUILD_DIR
 endif
