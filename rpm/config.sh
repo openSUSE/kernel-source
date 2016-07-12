a577f304d71d3a501d9c65cfb8a50c3779b21888
Michal Marek <mmarek@suse.com>
2016-07-12 13:02:23 +0200

    Automatically updated to 4.7-rc7-4-g08d27eb20666

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.7-rc6-94-gcc23c61
+SRCVERSION=4.7-rc7-4-g08d27eb20666
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
