9bb484563182a19ec80963af172e404a9c502d4a
Kernel Build Daemon <kbuild@suse.de>
2016-08-11 12:02:47 +0200

    Automatically updated to 4.8-rc1-80-g85e97be

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.8-rc1-53-ga0cba21
+SRCVERSION=4.8-rc1-80-g85e97be
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
