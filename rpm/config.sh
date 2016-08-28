45d2d79692e47e299210f17d077224f8ff6d6c7f
Kernel Build Daemon <kbuild@suse.de>
2016-08-28 12:10:55 +0200

    Automatically updated to 4.8-rc3-201-gaf56ff2

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.8-rc3-183-g5e608a0
+SRCVERSION=4.8-rc3-201-gaf56ff2
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
