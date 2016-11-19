03b826376f6fb9ed1a96ac7474fbc5a51b07fa7c
Kernel Build Daemon <kbuild@suse.de>
2016-11-19 12:04:11 +0100

    Automatically updated to 4.9-rc5-297-g20afa6e

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.9-rc5-264-g6238986
+SRCVERSION=4.9-rc5-297-g20afa6e
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
