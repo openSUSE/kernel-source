627003e83f35fe4a2a63a40d5f98b8f3b7f679b6
Michal Marek <mmarek@suse.com>
2017-01-03 15:51:45 +0100

    Automatically updated to 4.10-rc2-16-gc8b4ec8351d2

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.10-rc2-16-gc8b4ec8
+SRCVERSION=4.10-rc2-16-gc8b4ec8351d2
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
