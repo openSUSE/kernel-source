56e61bb1a92e8c12e737bc50d1f28d575253e1dd
Michal Marek <mmarek@suse.com>
2017-04-11 15:42:16 +0200

    Automatically updated to 4.11-rc6-4-gc08e611b7d01

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.11-rc6-4-gc08e611
+SRCVERSION=4.11-rc6-4-gc08e611b7d01
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
