b886c35bfae36cf43166135c84992338ff1b8a87
Kernel Build Daemon <kbuild@suse.de>
2017-05-29 12:03:20 +0200

    Automatically updated to 4.12-rc3

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.12-rc2-284-g249f1ef
+SRCVERSION=4.12-rc3
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
