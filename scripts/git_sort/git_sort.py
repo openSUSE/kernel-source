de89c2b3ee0b348dc5bf789d57957f6df77ce1ef
Johannes Thumshirn <jthumshirn@suse.de>
2018-04-10 11:17:13 +0200

    Add mkp's queue to git_sort.py

--- scripts/git_sort/git_sort.py
+++ scripts/git_sort/git_sort.py
@@ -200,6 +200,7 @@ remotes = (
     Head(RepoURL("mkp/scsi.git"), "4.15/scsi-fixes"),
     Head(RepoURL("mkp/scsi.git"), "4.16/scsi-fixes"),
     Head(RepoURL("mkp/scsi.git"), "4.17/scsi-queue"),
+    Head(RepoURL("mkp/scsi.git"), "queue"),
     Head(RepoURL("git://git.kernel.dk/linux-block.git"), "for-next"),
     Head(RepoURL("git://git.kernel.org/pub/scm/virt/kvm/kvm.git"), "queue"),
     Head(RepoURL("git://git.infradead.org/nvme.git"), "nvme-4.16-rc"),
