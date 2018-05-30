68fb80394a0866363cb986728ec5910b4c53b8c3
Michal Suchanek <msuchanek@suse.de>
2018-05-30 12:20:08 +0200

    Automatically updated to 4.17-rc7-31-g0044cdeb7313

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.17-rc7-31-g0044cde
+SRCVERSION=4.17-rc7-31-g0044cdeb7313
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
