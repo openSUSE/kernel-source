e30de3e6b9f4c894552f58c6210539d897d0d06c
Kernel Build Daemon <kbuild@suse.de>
2018-07-04 12:03:58 +0200

    Automatically updated to 4.18-rc3-113-gfc36def

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.18-rc3-107-gd0fbad0
+SRCVERSION=4.18-rc3-113-gfc36def
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
