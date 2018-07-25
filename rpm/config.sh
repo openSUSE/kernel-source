736d103565acb061ab21e42635f62aa67fe92155
Michal Suchanek <msuchanek@suse.de>
2018-07-25 13:35:00 +0200

    Automatically updated to 4.18-rc6-93-g9981b4fb8684

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.18-rc6-93-g9981b4f
+SRCVERSION=4.18-rc6-93-g9981b4fb8684
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
