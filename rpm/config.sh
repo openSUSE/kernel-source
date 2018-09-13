c034629da32391d74778808ab37fab5c5c5766bd
Kernel Build Daemon <kbuild@suse.de>
2018-09-13 12:01:51 +0200

    Automatically updated to 4.19-rc3-130-g54eda9d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.19-rc3-21-g5e33554
+SRCVERSION=4.19-rc3-130-g54eda9d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
