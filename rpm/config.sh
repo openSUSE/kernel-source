80aa112518cc58d5c0a3bcd5475b7d3a8389563a
Michal Kubecek <mkubecek@suse.cz>
2018-09-30 21:43:14 +0200

    Update to 4.19-rc6
    - Eliminated 2 patches

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.19-rc5
+SRCVERSION=4.19-rc6
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
