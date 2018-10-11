98bed5e35878757eda8161523c4e0f82086eb542
Kernel Build Daemon <kbuild@suse.de>
2018-10-11 12:04:18 +0200

    Automatically updated to 4.19-rc7-49-g9dcd936c5312

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.19-rc7-33-gbb2d8f2f6104
+SRCVERSION=4.19-rc7-49-g9dcd936c5312
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
