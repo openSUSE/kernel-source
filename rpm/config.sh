7852ac4302d67f632df8504b7d57738621f98761
Kernel Build Daemon <kbuild@suse.de>
2018-11-27 12:04:16 +0100

    Automatically updated to 4.20-rc4-7-gef78e5ec9214

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.20-rc4
+SRCVERSION=4.20-rc4-7-gef78e5ec9214
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
