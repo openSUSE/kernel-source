32b4ad8844b8789c78147349f793649756c6902e
Kernel Build Daemon <kbuild@suse.de>
2018-12-07 12:01:48 +0100

    Automatically updated to 4.20-rc5-86-gb72f711a4efa

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=4.20-rc5-62-gcf76c364a1e1
+SRCVERSION=4.20-rc5-86-gb72f711a4efa
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
