3aace103bd92cf3cd9842eb84444d37b1d7cf7ba
Kernel Build Daemon <kbuild@suse.de>
2019-03-01 12:02:50 +0100

    Automatically updated to 5.0-rc8-next-20190301

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.0-rc8-next-20190228
+SRCVERSION=5.0-rc8-next-20190301
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
