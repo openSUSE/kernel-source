3d5eb0031a05a16f713f4bc115e8edabb7dce425
Matthias Brugger <mbrugger@suse.com>
2019-04-25 17:24:25 +0200

    scripts/git_sort/git_sort.py: add repositories for arm-smmu-*

--- scripts/git_sort/git_sort.py
+++ scripts/git_sort/git_sort.py
@@ -235,6 +235,7 @@ remotes = (
     Head(RepoURL("djbw/nvdimm.git"), "libnvdimm-pending"),
     Head(RepoURL("git://git.linux-nfs.org/projects/anna/linux-nfs.git"), "linux-next"),
     Head(RepoURL("acme/linux.git"), "perf/core"),
+    Head(RepoURL("will/linux.git"), "for-joerg/arm-smmu/updates"),
     Head(RepoURL("herbert/crypto-2.6.git"), "master"),
     Head(RepoURL("git://git.cmpxchg.org/linux-mmots.git"), "master"),
     Head(RepoURL("git://git.linux-nfs.org/~bfields/linux.git"), "nfsd-next"),
