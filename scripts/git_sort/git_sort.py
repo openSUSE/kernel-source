c2096bbb6a409edb2cee38b463d5a197a1f665d0
Johannes Thumshirn <jthumshirn@suse.de>
2019-07-05 13:38:19 +0200

    scripts/git_sort/git_sort.py: add nvdimm/libnvdimm-fixes

--- scripts/git_sort/git_sort.py
+++ scripts/git_sort/git_sort.py
@@ -228,6 +228,7 @@ remotes = (
     Head(RepoURL("viro/vfs.git"), "fixes"),
     Head(RepoURL("jeyu/linux.git"), "modules-next"),
     Head(RepoURL("nvdimm/nvdimm.git"), "libnvdimm-for-next"),
+    Head(RepoURL("nvdimm/nvdimm.git"), "libnvdimm-fixes"),
     Head(RepoURL("djbw/nvdimm.git"), "libnvdimm-pending"),
     Head(RepoURL("herbert/crypto-2.6.git"), "master"),
 )
