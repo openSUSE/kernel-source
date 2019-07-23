b1bac7290e69cc5aabf67c58f271e905eb885030
Takashi Iwai <tiwai@suse.de>
2019-07-23 18:12:09 +0200

    rpm/config.sh: enable kernel module compression (bsc#1135854)

--- rpm/config.sh
+++ rpm/config.sh
@@ -4,6 +4,8 @@ SRCVERSION=5.3-rc4
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
 VANILLA_ONLY=1
+# enable kernel module compression
+COMPRESS_MODULES="xz"
 # buildservice projects to build the kernel against
 OBS_PROJECT=openSUSE:Factory
 OBS_PROJECT_ARM=openSUSE:Factory:ARM
