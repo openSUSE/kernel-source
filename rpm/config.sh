5d7acaad7389bcec8e5b58ce222d2d68b693bab7
Michal Kubecek <mkubecek@suse.cz>
2019-12-16 06:31:13 +0100

    Update to 5.5-rc2
    - update configs
      - CRYPTO_BLAKE2B=m

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.5-rc1
+SRCVERSION=5.5-rc2
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # enable kernel module compression
