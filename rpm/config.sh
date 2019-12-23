acd079750d5fb0e13c48572e02df06353f88000c
Michal Kubecek <mkubecek@suse.cz>
2019-12-23 07:25:16 +0100

    Update to 5.5-rc3
    - refresh configs

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.5-rc2
+SRCVERSION=5.5-rc3
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # enable kernel module compression
