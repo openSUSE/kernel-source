e5c81c17392e91388846cb69fecddaff049c7db1
Kernel Build Daemon <kbuild@suse.de>
2019-12-30 12:02:08 +0100

    Automatically updated to 5.5-rc4

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.5-rc3-62-gbf8d1cd43865
+SRCVERSION=5.5-rc4
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
