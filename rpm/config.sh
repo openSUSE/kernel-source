e8551bcd43c4fc45281cbb0a5d1a46f44ba0cc98
Kernel Build Daemon <kbuild@suse.de>
2020-06-12 08:34:10 +0200

    Automatically updated to 5.7-next-20200612

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.7-next-20200611
+SRCVERSION=5.7-next-20200612
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
