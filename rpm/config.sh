5f98d4180c4437fcd3bfd98f61cb8ecca102da4d
Kernel Build Daemon <kbuild@suse.de>
2021-02-23 12:04:10 +0100

    Automatically updated to 5.11-9786-g3b9cdafb5358

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.11-7579-g31caf8b2a847
+SRCVERSION=5.11-9786-g3b9cdafb5358
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
