f1e7bc3e03f8d9d393a139d3eca3d4d493e07dae
Jiri Slaby <jslaby@suse.cz>
2021-03-03 12:28:08 +0100

    scripts/run_oldconfig.sh: make use of scripts/dummy-tools (bcs#1181862).
    scripts/dummy-tools is a cross-toolchain from the kernel which
    advertises support for _everything_ (on the toolchain side). Using
    these, we obtain super-configs which are then reduced during build time
    when real toolchain (like gcc, ld, ...) is used.
    
    This allows us to drop the need for cross-compilers, specific versions
    of gcc etc. This is always pain as run_oldconfig.sh ran on different
    machines produces different configs.

--- scripts/run_oldconfig.sh
+++ scripts/run_oldconfig.sh
@@ -421,41 +421,7 @@ for config in $config_files; do
         MAKE_ARGS="ARCH=$cpu_arch"
         ;;
     esac
-    unset cross_arch
-    unset cross_extra
-    case $config in
-    arm64/*)
-	cross_arch="aarch64"
-	;;
-    arm*/*)
-	cross_arch="arm"
-        cross_extra="gnueabi-"
-	;;
-    ppc64le/*)
-	cross_arch="powerpc64le"
-	;;
-    ppc64/*)
-	cross_arch="powerpc64"
-	;;
-    ppc/*)
-        cross_arch="powerpc"
-	;;
-    i386/*)
-	# hack: whatever i386-suse-linux-gcc is, it does not support asm-goto
-	cross_arch="x86_64"
-	;;
-    *)
-        cross_arch="${config%%/*}"
-	;;
-    esac
-    [ "$cross_arch" = "$(uname -m)" ] && cross_arch=
-    cross_compile="${CROSS_COMPILE-${cross_arch}-suse-linux-${cross_extra}}"
-    if [ -n "$cross_arch" -a -x /usr/bin/${cross_compile}gcc ]; then
-	MAKE_ARGS="$MAKE_ARGS CROSS_COMPILE=$cross_compile"
-    fi
-    if [ -n "$CC" -a -z "$cross_arch" ]; then
-        MAKE_ARGS="$MAKE_ARGS CC=$CC"
-    fi
+    MAKE_ARGS="$MAKE_ARGS CROSS_COMPILE=scripts/dummy-tools/"
     if $silent; then
 	    MAKE_ARGS="$MAKE_ARGS -s"
     fi
