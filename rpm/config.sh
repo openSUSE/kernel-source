641dff8d2ad21fe61b4769d3629887bfc413b96b
Michal Suchanek <msuchanek@suse.de>
2021-07-08 12:08:36 +0200

    rpm/config.sh: Build on s390.

--- rpm/config.sh
+++ rpm/config.sh
@@ -14,6 +14,7 @@ OBS_PROJECT=openSUSE:Factory
 OBS_PROJECT_ARM=openSUSE:Factory:ARM
 OBS_PROJECT_PPC=openSUSE:Factory:PowerPC
 OBS_PROJECT_RISCV=openSUSE:Factory:RISCV
+OBS_PROJECT_S390=openSUSE:Factory:zSystems
 IBS_PROJECT=SUSE:Factory:Head
 IBS_PROJECT_ARM=Devel:ARM:Factory
 # Bugzilla info
