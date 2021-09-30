5cfcea90d5ee656b951ebb7fa45e3a853ee6238b
Kernel Build Daemon <kbuild@suse.de>
2021-09-30 12:02:55 +0200

    Automatically updated to 5.15-rc3-135-g02d5e016800d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.15-rc3-104-ga4e6f95a891a
+SRCVERSION=5.15-rc3-135-g02d5e016800d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
