20b8fdda8447b48dd906839cb90c0da55b49013a
Kernel Build Daemon <kbuild@suse.de>
2022-02-21 12:11:10 +0100

    Automatically updated to 5.17-rc5

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.17-rc4-229-g4f12b742eb2b
+SRCVERSION=5.17-rc5
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
