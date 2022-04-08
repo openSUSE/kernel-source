openSUSE/kernel-source

commit eaff9bcc7268adc7168483c0772f8fa6b3ab473b

git_sort/lib: Only allow patches intended for mainline.
Signed-off-by: Michal Suchanek <msuchanek@suse.de>

scripts/git_sort/lib.py
@@ -267,11 +267,25 @@ class InputEntry(object):
             raise exc.KSError("Could not find patch \"%s\"" % (name,))
 
         with Patch(open(name, mode="rb")) as patch:
+            mainline_tags = patch.get("Patch-mainline")
             commit_tags = patch.get("Git-commit")
             repo_tags = patch.get("Git-repo")
 
+        if len(repo_tags) > 1:
+            raise exc.KSError("Multiple Patch-mainline tags found. Patch \"%s\" is "
+                          "tagged improperly." % (name,))
+
         if not commit_tags:
             self.dest_head = git_sort.oot
+            mainline = mainline_tags[0]
+            if not re.match("^(Submitted|Not yet)", mainline, re.IGNORECASE):
+                raise exc.KSError(
+                    "There is a problem with patch \"%s\". "
+                    "The Patch-mainline tag \"%s\" is not supported in sorted "
+                    "section. Please add the patches without a commit id that "
+                    "are neither 'Submitted' nor 'Not yet' submitted to the "
+                    "manually maintained section below sorted section." % (
+                        name, mainline,))
             return
 
         class BadTag(Exception):

scripts/git_sort/tests/support.py
@@ -75,7 +75,7 @@ def format_patch(commit, mainline=None, repo=None, references=None,
             f.write("Git-repo: %s\n" % (repo,))
             f.write("Git-commit: %s\n" % (str(commit.id),))
         else:
-            f.write("Patch-mainline: No\n")
+            f.write("Patch-mainline: Not yet, to be submitted by partner developer\n")
         if references is not None:
             f.write("References: %s\n" % (references,))
         f.write("Subject: %s" % (commit.message,))

commit 24354fd8aa8aa3fcf21cc0aa5c8237e3f4eabce3

git_sort: Fix error message for patches missing Git-commit.
To reject unsortable patches from out-of-tree section patches without a
Git-commit that don't have Patch-mainline Submitted or Not yet are
rejected with an error message saying that this tag is not supported.

However, this is the case also for patches that have Patch-mainline
Queued or version which are missing Git-commit.

Add a separate error message for this case.

Fixes: eaff9bcc7268 ("git_sort/lib: Only allow patches intended for mainline.")
Signed-off-by: Michal Suchanek <msuchanek@suse.de>

scripts/git_sort/lib.py
@@ -280,6 +280,11 @@ class InputEntry(object):
         if not commit_tags:
             self.dest_head = git_sort.oot
             mainline = mainline_tags[0]
+            if re.match("^(v[1-9]|Queued)", mainline, re.IGNORECASE):
+                raise exc.KSError(
+                    "There is a problem with patch \"%s\". "
+                    "The Patch-mainline tag \"%s\" requires Git-commit." % (
+                        name, mainline,))
             if not re.match("^(Submitted|Not yet)", mainline, re.IGNORECASE):
                 raise exc.KSError(
                     "There is a problem with patch \"%s\". "

scripts/git_sort/tests/test_series_sort.py
@@ -955,6 +955,18 @@ class TestFromPatch(unittest.TestCase):
 "Error: There is a problem with patch \"%s\". The Git-repo tag is incorrect or the patch is in the wrong section of series.conf and (the Git-commit tag is incorrect or the relevant remote is outdated or not available locally) or an entry for this repository is missing from \"remotes\". In the last case, please edit \"remotes\" in \"scripts/git_sort/git_sort.py\" and commit the result. Manual intervention is required.\n" % (name,))
 
 
+    def test_malformed(self):
+        """
+        Generate a series and destroy the Git-commit tag on one of the patches
+        This should report a specific error so that this situation is not conflated with wrong Patch-mainline tag in out-of-tree section
+        """
+
+        name, series2 = self.prepare_found_indexed_upstream_good()
+        subprocess.call(['sed', '-i', '-e', 's/commit/comit/', name])
+        self.check_failure(
+'Error: There is a problem with patch "%s". The Patch-mainline tag "Queued in subsystem maintainer repository" requires Git-commit.\n' % (name))
+
+
 if __name__ == '__main__':
     # Run a single testcase
     suite = unittest.TestLoader().loadTestsFromTestCase(TestFromPatch)