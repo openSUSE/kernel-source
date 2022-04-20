a311575923f1df47e165aecd5c5c3f3615e77407
Kernel Build Daemon <kbuild@suse.de>
2022-04-20 12:11:52 +0200

    Automatically updated to 5.18-rc3-7-g559089e0a93d

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=5.18-rc3
+SRCVERSION=5.18-rc3-7-g559089e0a93d
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # Set to 1 if the tree does not contain any non-vanilla patches at all
