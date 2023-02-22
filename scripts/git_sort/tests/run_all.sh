b2615b219f9c0bd9b1d73dbf126ba721a27ef791
Michal Suchanek <msuchanek@suse.de>
2023-02-22 18:08:49 +0100

    git_sort: tests: do not disable package repository GPG check
    This adds the Kernel repository key and enables GPG check for package
    installation inside containers.

--- scripts/git_sort/tests/run_all.sh
+++ scripts/git_sort/tests/run_all.sh
@@ -10,8 +10,10 @@ for release in \
 	opensuse-tumbleweed \
 	; do
 	echo "Building container image for $release..."
+	cp -a $libdir/../../lib/SUSE/Kernel.gpg $libdir/$release
 	docker build -q -t gs-test-$release "$libdir/$release"
 	ret=$?
+	rm -f $libdir/$release/Kernel.gpg
 	[ $ret -eq 0 ] || exit $?
 	echo "Running tests in $release:"
 	docker run --rm --name=gs-test-$release \
