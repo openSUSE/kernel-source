76638c4c1324116d7b1d021297d3979f8c9b4d48
Takashi Iwai <tiwai@suse.de>
2023-08-17 11:07:18 +0200

    rpm/config.sh: disable supported.conf check temporarily for 6.4 updates

--- rpm/config.sh
+++ rpm/config.sh
@@ -18,7 +18,7 @@ BUGZILLA_PRODUCT="SUSE Linux Enterprise Server 15 SP6"
 # Check the sorted patches section of series.conf
 SORT_SERIES=yes
 # Modules not listed in supported.conf will abort the kernel build
-SUPPORTED_MODULES_CHECK=Yes
+SUPPORTED_MODULES_CHECK=No
 # Split Leap-only modules to kernel-*-optional subpackage
 SPLIT_OPTIONAL=Yes
 # build documentation in HTML format
