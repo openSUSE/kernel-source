8f5995d2fe05d4e5e0387cac4e8bf6f48551ba71
Michal Kubecek <mkubecek@suse.cz>
2023-10-15 22:46:40 +0200

    Update to 6.6-rc6
    - refresh configs

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=6.6-rc5
+SRCVERSION=6.6-rc6
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # enable kernel module compression
