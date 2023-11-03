12ef2b62b36b669a1c713843f7a22dfe0ffbe9e2
Kernel Build Daemon <kbuild@suse.de>
2023-11-03 12:12:55 +0100

    Automatically updated to 6.6-12401-g8f6f76a6a29f

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=6.6-10297-g21e80f3841c0
+SRCVERSION=6.6-12401-g8f6f76a6a29f
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=-vanilla
 # Set to 1 to use the variant kernel for kernel-obs-build
