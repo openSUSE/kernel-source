a9d4e0a5b55bfeaea3b694d8033486e59e8756b8
Michal Suchanek <msuchanek@suse.de>
2023-12-01 14:43:31 +0100

    rpm/config.sh: Remove unsupported architecture build projects

--- rpm/config.sh
+++ rpm/config.sh
@@ -11,12 +11,7 @@ BUILD_DTBS="Yes"
 LIVEPATCH=livepatch
 # buildservice projects to build the kernel against
 OBS_PROJECT=openSUSE:Factory
-OBS_PROJECT_ARM=openSUSE:Factory:ARM
-OBS_PROJECT_PPC=openSUSE:Factory:PowerPC
-OBS_PROJECT_RISCV=openSUSE:Factory:RISCV
-OBS_PROJECT_S390=openSUSE:Factory:zSystems
 IBS_PROJECT=SUSE:Factory:Head
-IBS_PROJECT_ARM=Devel:ARM:Factory
 # Bugzilla info
 BUGZILLA_SERVER="apibugzilla.suse.com"
 BUGZILLA_PRODUCT="openSUSE Tumbleweed"
