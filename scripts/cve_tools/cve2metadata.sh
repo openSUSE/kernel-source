e3ddb5d5c1573c0db7c22792812a831cdb809f19
Michal Hocko <mhocko@suse.com>
2024-03-25 15:51:40 +0100

    scripts/cve_tools/cve2metadata.sh: s@VULNS_GIT_PATH@VULNS_GIT@
    to be more in line with git tree env vars

--- scripts/cve_tools/cve2metadata.sh
+++ scripts/cve_tools/cve2metadata.sh
@@ -4,11 +4,11 @@
 # cve2metadata.sh CVE-NUM[...CVE-NUM]
 #
 # expects:
-# VULNS_GIT_PATH to point to vulns DB git tree (clone from https://git.kernel.org/pub/scm/linux/security/vulns.git)
+# VULNS_GIT to point to vulns DB git tree (clone from https://git.kernel.org/pub/scm/linux/security/vulns.git)
 
-if [ -z "$VULNS_GIT_PATH" -o ! -d "$VULNS_GIT_PATH" ]
+if [ -z "$VULNS_GIT" -o ! -d "$VULNS_GIT" ]
 then
-	echo "VULNS_GIT_PATH should point to vulns git tree" >&2
+	echo "VULNS_GIT should point to vulns git tree" >&2
 	echo "clone from https://git.kernel.org/pub/scm/linux/security/vulns.git" >&2
 	exit 1
 fi
@@ -18,7 +18,7 @@ fi
 while [ $# -gt 0 ]
 do
 	arg=$1
-	cve_sha="$(cd $VULNS_GIT_PATH; scripts/cve_search $arg 2>/dev/null | cut -d" " -f1,7)"
+	cve_sha="$(cd $VULNS_GIT; scripts/cve_search $arg 2>/dev/null | cut -d" " -f1,7)"
 	cve=${cve_sha%% *}
 	sha=${cve_sha##* }
 	if [ $(echo $sha | wc -c) -eq 41 ]
