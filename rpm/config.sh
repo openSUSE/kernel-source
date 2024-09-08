5661bfe97f7cd7d5434d8b832d5ae001a24101b6
Michal Kubecek <mkubecek@suse.cz>
2024-09-09 00:01:32 +0200

    update to 6.11-rc7
    - refresh configs

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=6.11-rc6
+SRCVERSION=6.11-rc7
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # enable kernel module compression
