78175ea6ca4ac148a328fabd4b5ce50ce12053a0
Michal Kubecek <mkubecek@suse.cz>
2024-09-15 23:18:36 +0200

    update to 6.11 final
    - refresh configs (headers only)

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,5 +1,5 @@
 # The version of the main tarball to use
-SRCVERSION=6.11-rc7
+SRCVERSION=6.11
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # enable kernel module compression
