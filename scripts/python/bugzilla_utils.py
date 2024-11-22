68b5d1c1a19bb3c549399b62a2dab874cfc8d3d8
Miroslav Franc <mfranc@suse.cz>
2024-11-22 10:24:31 +0100

    get-creation-dates: fetch creation date based on a bug id or a CVE number
    It is a standalone script used for CVE reporting.

---
+++ scripts/python/bugzilla_utils.py
@@ -0,0 +1,25 @@
+import bugzilla, datetime
+from bugzilla._cli import DEFAULT_BZ
+
+def get_bugzilla_api():
+    return bugzilla.Bugzilla(DEFAULT_BZ)
+
+def check_being_logged_in(bzapi):
+    if not bzapi.logged_in:
+        print("You are not logged in the bugzilla!\n\nGo to https://bugzilla.suse.com/, log in via web interace with your credentials.\n"\
+             "Then go to Preferences, click on the tab API KEYS and generate a new api key\nif you don't have one already.  Then store "\
+             "the api_key in a file ~/.bugzillarc\nin the following format...\n\n# ~/.bugzillarc\n[apibugzilla.suse.com]\napi_key = YOUR_API_KEY")
+        return False
+    return True
+
+def make_unique(alist):
+    try:
+        return { c for c in alist if c.startswith('CVE-') }.pop()
+    except:
+        return ''
+
+def make_url(bug_id):
+    return f'https://bugzilla.suse.com/show_bug.cgi?id={bug_id}'
+
+def format_time(t):
+    return datetime.datetime.strptime(str(t), '%Y%m%dT%H:%M:%S')
